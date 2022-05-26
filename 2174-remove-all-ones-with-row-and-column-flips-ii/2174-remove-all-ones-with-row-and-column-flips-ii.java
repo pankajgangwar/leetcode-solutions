1e08609462006cc4b33ba6fb5184dd54dae8fbad
Pankaj Kumar <pankaj.gat@gmail.com>
2022-05-26 08:11:37 +0530

    Time: 1 ms (78.76%), Space: 40.1 MB (81.37%) - LeetHub

---
+++ 2174-remove-all-ones-with-row-and-column-flips-ii/2174-remove-all-ones-with-row-and-column-flips-ii.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int removeOnes(int[][] grid) {
+        int res = Integer.MAX_VALUE;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if(grid[i][j] == 0){
+                    continue;
+                }
+                List<Integer> x = new ArrayList<>();
+                for (int k = 0; k < grid.length; k++) {
+                    if(grid[k][j] == 1){
+                        x.add(k);
+                        grid[k][j] = 0;
+                    }
+                    
+                }
+                List<Integer> y = new ArrayList<>();
+                for (int k = 0; k < grid[0].length; k++) {
+                    if(grid[i][k] == 1){
+                        y.add(k);
+                        grid[i][k] = 0;
+                    }
+                }
+                res = Math.min(res , 1 + (removeOnes(grid)));
+                for (int k : x) {
+                    grid[k][j] = 1;
+                }
+                for (int k : y) {
+                    grid[i][k] = 1;
+                }
+            }
+        }
+        return res == Integer.MAX_VALUE ? 0 : res;
+    }
+
+}
