2fb7b8b39c0973b6cf1acc99d0883bf34e2e3168
Pankaj Kumar <pankaj.gat@gmail.com>
2022-05-26 07:08:18 +0530

    Time: 12 ms (71.67%), Space: 41.2 MB (53.20%) - LeetHub

--- 762-prime-number-of-set-bits-in-binary-representation/762-prime-number-of-set-bits-in-binary-representation.java
+++ 762-prime-number-of-set-bits-in-binary-representation/762-prime-number-of-set-bits-in-binary-representation.java
@@ -1,9 +1,10 @@
 class Solution {
     public int countPrimeSetBits(int left, int right) {
         int count = 0;
+        int[] arr = new int[]{2, 3, 5, 7, 11, 13, 17, 19};
         for(int i = left; i <= right; i++){
             int bits = Integer.bitCount(i);
-            if(isPrime(bits)){
+            if(Arrays.binarySearch(arr, bits) >= 0){
                 count += 1;
             }
         }
