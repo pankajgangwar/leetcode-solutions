00e1f573b31294c82602b7988a5736bbd9c2d3b4
Pankaj Kumar <pankaj.gat@gmail.com>
2022-05-26 08:48:05 +0530

    Time: 1 ms (78.76%), Space: 41.4 MB (63.07%) - LeetHub

--- 2174-remove-all-ones-with-row-and-column-flips-ii/2174-remove-all-ones-with-row-and-column-flips-ii.java
+++ 2174-remove-all-ones-with-row-and-column-flips-ii/2174-remove-all-ones-with-row-and-column-flips-ii.java
@@ -5,12 +5,12 @@ class Solution {
         int c = grid[0].length;
         int[] cols = new int[r];
         int[] rows = new int[c];
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[i].length; j++) {
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
                 if(grid[i][j] == 0){
                     continue;
                 }
-                /*for (int k = 0; k < r; k++) {
+                for (int k = 0; k < r; k++) {
                     cols[k] = grid[k][j];
                     grid[k][j] = 0;
                 }
@@ -20,35 +20,12 @@ class Solution {
                 }
                 res = Math.min(res , 1 + (removeOnes(grid)));
                 //Backtrack
-                for (int k = 0; k < r; k++) {
-                    grid[k][j] = cols[k];
-                }
-                for (int k = 0; k < c; k++) {
-                    grid[i][k] = rows[k];
-                }*/
-                List<Integer> x = new ArrayList<>();
-                for (int k = 0; k < grid.length; k++) {
-                    if(grid[k][j] == 1){
-                        x.add(k);
-                        grid[k][j] = 0;
-                    }
-                    
-                }
-                List<Integer> y = new ArrayList<>();
-                for (int k = 0; k < grid[0].length; k++) {
-                    if(grid[i][k] == 1){
-                        y.add(k);
-                        grid[i][k] = 0;
-                    }
-                }
-                res = Math.min(res , 1 + (removeOnes(grid)));
-                //Backtrack
-                for (int k : x) {
-                    grid[k][j] = 1;
-                }
-                for (int k : y) {
-                    grid[i][k] = 1;
-                }
+               for (int k = 0; k < c; k++) {
+	            grid[i][k] = rows[k];
+	       }
+	       for (int k = 0; k < r; k++) {
+	            grid[k][j] = cols[k];
+	       }
             }
         }
         return res == Integer.MAX_VALUE ? 0 : res;
