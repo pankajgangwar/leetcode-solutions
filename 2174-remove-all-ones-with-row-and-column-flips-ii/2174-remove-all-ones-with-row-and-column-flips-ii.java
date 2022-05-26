2eb197f6da682ae8a7070b7e8b35114edbb851f1
Pankaj Kumar <pankaj.gat@gmail.com>
2022-05-26 08:38:15 +0530

    Time: 1 ms (78.76%), Space: 42.2 MB (15.36%) - LeetHub

--- 2174-remove-all-ones-with-row-and-column-flips-ii/2174-remove-all-ones-with-row-and-column-flips-ii.java
+++ 2174-remove-all-ones-with-row-and-column-flips-ii/2174-remove-all-ones-with-row-and-column-flips-ii.java
@@ -1,11 +1,31 @@
 class Solution {
     public int removeOnes(int[][] grid) {
         int res = Integer.MAX_VALUE;
+        int r = grid.length;
+        int c = grid[0].length;
+        int[] cols = new int[r];
+        int[] rows = new int[c];
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
                 if(grid[i][j] == 0){
                     continue;
                 }
+                /*for (int k = 0; k < r; k++) {
+                    cols[k] = grid[k][j];
+                    grid[k][j] = 0;
+                }
+                for (int k = 0; k < c; k++) {
+                    rows[k] = grid[i][k];
+                    grid[i][k] = 0;
+                }
+                res = Math.min(res , 1 + (removeOnes(grid)));
+                //Backtrack
+                for (int k = 0; k < r; k++) {
+                    grid[k][j] = cols[k];
+                }
+                for (int k = 0; k < c; k++) {
+                    grid[i][k] = rows[k];
+                }*/
                 List<Integer> x = new ArrayList<>();
                 for (int k = 0; k < grid.length; k++) {
                     if(grid[k][j] == 1){
