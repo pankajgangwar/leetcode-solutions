30f6a3df2fda8eedc5d23d25a4a35b00cf8dfe3d
Pankaj Kumar <pankaj.gat@gmail.com>
2022-05-26 11:01:47 +0530

    Time: 59 ms (70.70%), Space: 73.3 MB (65.07%) - LeetHub

--- 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.java
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.java
@@ -1,24 +1,51 @@
 class Solution {
-
+    
     public List<String> findAllRecipes(String[] recipes, List<List<String>> ingredients, String[] supplies) {
+        return topologicalSort(recipes, ingredients, supplies);
+    }
+
+    public List<String> topologicalSort(String[] recipes, List<List<String>> ingredients, String[] supplies){
+        HashMap<String, List<String>> graph = new HashMap<>();
         HashSet<String> sup = new HashSet<>(Arrays.asList(supplies));
-        HashMap<String, Integer> indexMap = new HashMap<>();
+        HashMap<String,Integer> indegree = new HashMap<>();
+
         for (int i = 0; i < recipes.length; i++) {
-            indexMap.put(recipes[i], i);
+            indegree.put(recipes[i], 0);
+        }
+
+        for (int i = 0; i < recipes.length; i++) {
+            for(String dep : ingredients.get(i)){
+                if(!sup.contains(dep)){
+                    graph.putIfAbsent(dep, new ArrayList<>());
+                    graph.get(dep).add(recipes[i]);
+                    indegree.put(recipes[i], indegree.getOrDefault(recipes[i], 0) + 1);
+                }
+            }
+        }
+        Queue<String> bfs = new LinkedList<>();
+        for(String key : indegree.keySet()){
+            if(indegree.get(key) == 0){
+                bfs.offer(key);
+            }
         }
         List<String> res = new ArrayList<>();
-        for (int i = 0; i < recipes.length; i++) {
-            String r = recipes[i];
-            if (helper(r, indexMap, ingredients, sup)) {
-                res.add(r);
+        while (!bfs.isEmpty()){
+            String curr = bfs.poll();
+            res.add(curr);
+            if(!graph.containsKey(curr)) continue;
+            for(String dep : graph.get(curr)){
+                indegree.put(dep, indegree.get(dep) - 1);
+                if(indegree.get(dep) == 0){
+                    bfs.offer(dep);
+                }
             }
         }
         return res;
     }
 
-    HashMap<String, Boolean> visited = new HashMap<>();
-
-    public boolean helper(String r, HashMap<String, Integer> indexMap, List<List<String>> ingredients, HashSet<String> supplies) {
+    HashMap<String,Boolean> visited = new HashMap<>();
+    public boolean helper(String r, HashMap<String, Integer> indexMap,
+                          List<List<String>> ingredients, HashSet<String> supplies){
         if(supplies.contains(r)) return true;
         if(visited.containsKey(r)) return visited.get(r);
         if(!indexMap.containsKey(r)) return false;
