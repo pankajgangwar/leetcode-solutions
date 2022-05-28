55acccccd28c84e7684d9aa46e54ededb9301ecd
Pankaj Kumar <pankaj.gat@gmail.com>
2022-05-28 10:56:05 +0530

    Time: 826 ms (16.64%), Space: 96.8 MB (26.72%) - LeetHub

--- 1554-strings-differ-by-one-character/1554-strings-differ-by-one-character.java
+++ 1554-strings-differ-by-one-character/1554-strings-differ-by-one-character.java
@@ -1,60 +1,46 @@
 class Solution {
     
-    class TrieNode {
-        Map<Character, TrieNode> children;
-        boolean end;
-        public TrieNode() {
-            children = new HashMap<>();
-        }
-    }
-    
-    public boolean differByOne(String[] dict) {
-        TrieNode root = buildTrie(dict);
-        for (String w : dict) {
-            if (search(root, w, 0, 0)) {
-                return true;
+     class Trie {
+        Trie[] children = new Trie[26];
+        boolean isEnd = false;
+
+        public void insert(String s, int idx) {
+            if(idx == s.length()){
+                isEnd = true;
+                return;
             }
+            int ch = s.charAt(idx) - 'a';
+            if(children[ch] == null){
+                children[ch] = new Trie();
+            }
+            children[ch].insert(s, idx + 1);
         }
-        return false;
-    }
-    
-    public boolean search (TrieNode root, String w, int diff, int index) {
-        if (diff > 1) {
-            return false;
-        }
-        
-        if (index >= w.length()) {
-            return root.end && diff == 1;
-        }
-        
-        char c = w.charAt(index);
-        for (char key: root.children.keySet()) {
-            if (key == c ) {
-                if (search(root.children.get(key), w, diff, index+1)) {
-                    return true;
-                } 
-            } else {
-                if (search(root.children.get(key), w, diff+1, index+1)) {
-                    return true;
+
+        public boolean find(String s, int index, int diff) {
+            if(diff > 1) return false;
+            if(index == s.length()) return (diff == 1 && isEnd);
+            int ch = s.charAt(index) - 'a';
+            boolean res = children[ch] != null && children[ch].find(s, index + 1, diff);
+            if(res) return true;
+            for(Trie curr : children){
+                if(curr != null && curr != children[ch]){
+                    if(curr.find(s, index + 1, diff + 1)){
+                        return true;
+                    }
                 }
             }
+            return false;
+        }
+    }
+
+    public boolean differByOne(String[] dict) {
+        Trie root = new Trie();
+        for(String s : dict){
+            if(root.find(s, 0, 0)){
+                return true;
+            }
+            root.insert(s,0);
         }
         return false;
-        
-    }
-    
-    public TrieNode buildTrie(String[] dict) {
-        TrieNode root = new TrieNode();
-        for (String word : dict) {
-            TrieNode node = root;
-            for (Character c : word.toCharArray()) {
-                if (!node.children.containsKey(c)) {
-                    node.children.put(c, new TrieNode());
-                } 
-                node = node.children.get(c);
-            }
-            node.end = true;
-        }
-        return root;
     }
 }
