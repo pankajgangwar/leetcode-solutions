4281962345aaf716c977104453e205e762295efe
Pankaj Kumar <pankaj.gat@gmail.com>
2022-05-29 21:27:55 +0530

    Time: 3 ms (92.56%), Space: 42.9 MB (76.77%) - LeetHub

--- 818-race-car/818-race-car.java
+++ 818-race-car/818-race-car.java
@@ -1,28 +1,9 @@
 class Solution {
     
      public int racecar(int t) {
-         return racecarBfs(t);
+         return racecarDp(t);
+         
      }
-    
-    public int racecarBfs1(int target) {
-        Queue<int[]> bfs = new LinkedList<>();
-        int[] start = new int[]{0,0,1,};
-        bfs.offer(start); // pos, speed
-        while (!bfs.isEmpty()){
-            int[] curr = bfs.poll();
-            int moves = curr[0];
-            int pos = curr[1];
-            int vel = curr[2];
-            if(pos == target) return moves;
-            if(Math.abs(pos) > 2 * target) continue;
-            bfs.offer(new int[]{moves + 1, pos + vel, vel * 2});
-            if(((pos + vel) > target && vel > 0) || (pos + vel < target && vel < 0)){
-                bfs.offer(new int[]{moves+1, pos, vel > 0 ? -1 : 1});
-            }
-        }
-        return 0;
-    }
-    
     int dp[] = new int[10001];
     public int racecarDp(int t) {
         if(dp[t] > 0) return dp[t];
@@ -50,19 +31,15 @@ class Solution {
             while (size-- > 0){
                 int[] curr = bfs.poll();
                 if(curr[0] == target) return level;
-                if(Math.abs(curr[0]) > 2 * target) continue;
-                int pos = curr[0];
-                int vel = curr[1];
-                int[] opt1 = new int[]{curr[0] + curr[1], curr[1] * 2};
-                int[] opt2 = new int[]{curr[0], curr[1] > 0 ? -1 : 1};
-                if(Math.abs(opt1[0] - target) < target && !visited.contains(Arrays.toString(opt1))){
-                    visited.add(Arrays.toString(opt1));
-                    bfs.offer(opt1);
+                int[] op1 = new int[]{curr[0] + curr[1], curr[1] * 2};
+                int[] op2 = new int[]{curr[0], curr[1] > 0 ? -1 : 1};
+                if(Math.abs(op1[0] - target) < target && !visited.contains(Arrays.toString(op1))){
+                    visited.add(Arrays.toString(op1));
+                    bfs.offer(op1);
                 }
-                
-                if(Math.abs(opt2[0] - target) < target && !visited.contains(Arrays.toString(opt2))){
-                    visited.add(Arrays.toString(opt2));
-                    bfs.offer(opt2);
+                if(Math.abs(op2[0] - target) < target && !visited.contains(Arrays.toString(op2))){
+                    visited.add(Arrays.toString(op2));
+                    bfs.offer(op2);
                 }
             }
             level += 1;
