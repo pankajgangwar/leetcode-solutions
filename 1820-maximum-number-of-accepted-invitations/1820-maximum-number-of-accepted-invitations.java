16f00b45a51a19ad180d5637c03a5327ffba40f2
Pankaj Kumar <pankaj.gat@gmail.com>
2022-05-31 08:20:32 +0530

    Time: 110 ms (28.44%), Space: 80.2 MB (32.11%) - LeetHub

---
+++ 1820-maximum-number-of-accepted-invitations/1820-maximum-number-of-accepted-invitations.java
@@ -0,0 +1,33 @@
+class Solution {
+
+    public int maximumInvitations(int[][] grid) {
+        int boys = grid.length;
+        int girls = grid[0].length;
+        int[] girlFixed = new int[girls];
+        Arrays.fill(girlFixed, -1);
+        int invitations = 0;
+        for (int i = 0; i < boys; i++) {
+            if (match(i, new HashSet<>(), grid, girlFixed)) {
+                ++invitations;
+            }
+        }
+        return invitations;
+    }
+
+    public boolean match(int boy, HashSet<Integer> seenGirl, int[][] grid, int[] girlFixed) {
+        int m = grid.length; // boys
+        int n = grid[0].length; // girls
+
+        for (int g = 0; g < n; g++) {
+            if(grid[boy][g] == 1 && !seenGirl.contains(g)){
+                seenGirl.add(g);
+                if(girlFixed[g] == -1 || match(girlFixed[g], seenGirl, grid, girlFixed)){
+                    girlFixed[g] = boy;
+                    return true;
+                }
+                //seenGirl.remove(g);
+            }
+        }
+        return false;
+    }
+}
