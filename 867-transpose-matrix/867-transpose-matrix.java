03449b6cf5dc91a2b9d5b5db1ca8d1583a7f6920
Pankaj Kumar <pankaj.gat@gmail.com>
2022-06-02 21:05:49 +0530

    Time: 1 ms (52.16%), Space: 49 MB (16.19%) - LeetHub

--- 867-transpose-matrix/867-transpose-matrix.java
+++ 867-transpose-matrix/867-transpose-matrix.java
@@ -1,39 +1,13 @@
 class Solution {
     public int[][] transpose(int[][] grid) {
-        int i = 0, j = 0;
         int rows = grid.length;
         int cols = grid[0].length;
         int[][] res = new int[cols][rows];
-        while (i < rows && j < cols){
-            int tempr = 0;
-            int tempc = 0;
-            while (tempr <= i && tempc <= j){
-                int first = grid[tempr][j];
-                int second = grid[i][tempc];
-                res[tempr][j] = second;
-                res[i][tempc] = first;
-                tempr++;
-                tempc++;
-            }
-            i++;
-            j++;
-        }
-        if(rows == cols){
-            return res;
-        }else{
-            res = new int[cols][rows];
-            int tempR = 0, tempC = 0;
-            for (int k = 0; k < rows; k++) {
-                for (int l = 0; l < cols; l++) {
-                    if(tempR == cols){
-                        tempR = 0;
-                        tempC++;
-                    }
-                    res[tempR++][tempC] = grid[k][l];
-                }
+        for (int i = 0; i < cols; i++) {
+            for (int j = 0; j < rows; j++) {
+                res[i][j] = grid[j][i];
             }
         }
-
         return res;
     }
 }
