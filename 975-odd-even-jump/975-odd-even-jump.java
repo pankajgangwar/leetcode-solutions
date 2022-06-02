82c9a4d560007e2481af48c732f05e32cc9bf16b
Pankaj Kumar <pankaj.gat@gmail.com>
2022-06-02 08:47:47 +0530

    Time: 111 ms (33.53%), Space: 54 MB (73.33%) - LeetHub

--- 975-odd-even-jump/975-odd-even-jump.java
+++ 975-odd-even-jump/975-odd-even-jump.java
@@ -3,6 +3,10 @@ class Solution {
         TreeMap<Integer, Integer> map = new TreeMap<>();
         int n = arr.length;
         boolean[][] dp = new boolean[n][2];
+        /*
+        dp[i][0] stands for you can arrive index n - 1 starting from index i at an odd step.
+        dp[i][1] stands for you can arrive index n - 1 starting from index i at an even step.
+        */
         dp[n-1][0] = true;
         dp[n-1][1] = true;
         map.put(arr[n-1], n-1);
@@ -17,7 +21,7 @@ class Solution {
                 dp[i][1] = dp[map.get(nextSmallerElement)][0];
             }
             map.put(arr[i], i);
-            res += dp[i][0] ? 1 : 0;
+            res += dp[i][0] ? 1 : 0; // Since we started with odd i.e 1
         }
         return res;
     }
