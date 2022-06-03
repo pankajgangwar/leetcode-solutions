0f568ba5d50edf60e4dc2474d4624f825bc0b685
Pankaj Kumar <pankaj.gat@gmail.com>
2022-06-03 08:06:24 +0530

    Time: 0 ms (100.00%), Space: 44.3 MB (12.58%) - LeetHub

---
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.java
@@ -0,0 +1,57 @@
+/**
+ * // This is MountainArray's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * interface MountainArray {
+ *     public int get(int index) {}
+ *     public int length() {}
+ * }
+ */
+
+class Solution {
+
+    public int findInMountainArray(int target, MountainArray A) {
+        int low = 0;
+        int n = A.length();
+        int peak = peakIndexInMountainArray(A);
+        int high = peak;
+        while (low <= high){
+            int mid = ( low + high )/ 2;
+            if(A.get(mid) == target){
+                return mid;
+            }else if(A.get(mid) > target){
+                high = mid - 1;
+            }else{
+                low = mid + 1;
+            }
+        }
+        low = peak;
+        high = n - 1;
+        while (low <= high){
+            int mid = ( low + high )/ 2;
+            if(A.get(mid) == target){
+                return mid;
+            }else if(A.get(mid) > target){
+                low = mid + 1;
+            }else{
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    public int peakIndexInMountainArray(MountainArray A) {
+        int l = 0, r = A.length() - 1, mid = 0;
+        while(l < r) {
+            mid = ( l + r )/ 2;
+            int m = A.get(mid);
+            if(m < A.get(mid + 1)) {
+                l = mid;
+            }else if(A.get(mid - 1) > m){
+                r = mid;
+            }else {
+                return mid;
+            }
+        }
+        return 0;
+    }
+}
