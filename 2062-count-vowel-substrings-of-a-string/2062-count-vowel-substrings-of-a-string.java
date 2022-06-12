ef9111e9db8dc699d2ebbb9496c85256ef916c8d
Pankaj Kumar <pankaj.gat@gmail.com>
2022-06-12 21:07:43 +0530

    Time: 7 ms (79.72%), Space: 42 MB (67.95%) - LeetHub

--- 2062-count-vowel-substrings-of-a-string/2062-count-vowel-substrings-of-a-string.java
+++ 2062-count-vowel-substrings-of-a-string/2062-count-vowel-substrings-of-a-string.java
@@ -1,5 +1,45 @@
 class Solution {
     public int countVowelSubstrings(String word) {
+        int ans = 0;
+        HashMap<Character, Integer> map = new HashMap<>();
+        map.put('a', 0);
+        map.put('e', 0);
+        map.put('i', 0);
+        map.put('o', 0);
+        map.put('u', 0);
+
+        int vow = 0, k = 0;
+
+        for (int end = 0, start = 0; end < word.length(); end++) {
+            char ch = word.charAt(end);
+            if(map.get(ch) != null){
+                map.put(ch, map.getOrDefault(ch, 0) + 1);
+                if(map.get(ch) == 1){
+                    ++vow;
+                }
+                while (vow == 5){
+                    char ss = word.charAt(k);
+                    map.put(ss, map.getOrDefault(ss, 0) - 1);
+                    if(map.get(ss) == 0){
+                        --vow;
+                    }
+                    k++;
+                }
+                ans += (k - start);
+            }else{
+                HashMap<Character, Integer> finalMap = map;
+                map.forEach((key, value) -> {
+                    finalMap.put(key, 0);
+                });
+                map = finalMap;
+                vow = 0;
+                k = start = end + 1;
+            }
+        }
+        return ans;
+    }
+    
+    public int countVowelSubstrings1(String word) {
         int n = word.length();
         String vowels = "aeiou";
         int res = 0;
