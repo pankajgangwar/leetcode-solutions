da0d16a7532c745a53f2f387c21f4b9ac4cb54a3
Pankaj Kumar <pankaj.gat@gmail.com>
2022-06-12 10:50:54 +0530

    Time: 350 ms (25.00%), Space: 41.6 MB (50.00%) - LeetHub

---
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int distributeCookies(int[] cookies, int k) {
+        helper(cookies, 0, k, new int[k]);
+        return res;
+    }
+
+    int res = Integer.MAX_VALUE;
+    public void helper(int[] cookies, int start, int k, int[] students){
+        if(start == cookies.length) {
+            //int max = Arrays.stream(students).max().getAsInt();
+            int max = 0;
+            for(int a : students) max = Math.max(max, a);
+            res = Math.min(res, max);
+            return;
+        }
+        for (int i = 0; i < k ; i++) {
+            students[i] += cookies[start];
+            helper(cookies, start + 1, k, students);
+            students[i] -= cookies[start];
+        }
+    }
+}
