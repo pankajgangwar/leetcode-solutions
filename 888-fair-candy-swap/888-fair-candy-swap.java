5e57aaff26aea32e05457c3a52401908affc997b
Pankaj Kumar <pankaj.gat@gmail.com>
2022-06-23 08:16:02 +0530

    Time: 25 ms (42.38%), Space: 73.2 MB (12.22%) - LeetHub

--- 888-fair-candy-swap/888-fair-candy-swap.java
+++ 888-fair-candy-swap/888-fair-candy-swap.java
@@ -1,18 +1,27 @@
 class Solution {
+    /*
+    Sa -A + B = Sb - B + A
+    2(A-B) = (Sa-Sb)
+    A-B = (Sa-Sb)/2
+    A = B+(Sa-Sb)/2
+    */
     public int[] fairCandySwap(int[] a, int[] b) {
         int aSum = Arrays.stream(a).sum();
         int bSum = Arrays.stream(b).sum();
         int diff = (aSum - bSum) / 2;
-        Arrays.sort(a);
+
+        //Arrays.sort(a);
+        HashSet<Integer> aSet = new HashSet<>();
+        for(int x : a) aSet.add(x);
         for (int num : b) {
-            int A = num + diff;
-            int B = Arrays.binarySearch(a, A);
+            /*int B = Arrays.binarySearch(a, num + diff);
             if(B >= 0){
-                return new int[]{A, num};
+                return new int[]{num + diff, num};
+            }*/
+            if(aSet.contains(num + diff)){
+                return new int[]{num + diff, num};
             }
         }
         return new int[]{};
     }
-    
-
 }
