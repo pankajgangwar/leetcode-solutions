9b204747f96bed1abd8b0e33455e01d1e692696f
Pankaj Kumar <pankaj.gat@gmail.com>
2022-06-26 12:39:41 +0530

    Time: 453 ms (80.00%), Space: 117.9 MB (60.00%) - LeetHub

--- 2320-count-number-of-ways-to-place-houses/2320-count-number-of-ways-to-place-houses.java
+++ 2320-count-number-of-ways-to-place-houses/2320-count-number-of-ways-to-place-houses.java
@@ -2,10 +2,10 @@ class Solution {
     public int countHousePlacements(int n) {
         long dp[][] = new long[n+1][4];
         // base case
-        dp[1][0] = 1;
-        dp[1][1] = 1;
-        dp[1][2] = 1;
-        dp[1][3] = 1;
+        dp[1][0] = 1; // not placed any house,
+        dp[1][1] = 1; // placed on one side of the street.
+        dp[1][2] = 1; // placed on the other side of the street.
+        dp[1][3] = 1; // placed on each side of the street.
         long mod = (long)1e9+7;
         for (int i = 2; i <= n; i++) {
             dp[i][0] = Arrays.stream(dp[i-1]).sum() % mod;
