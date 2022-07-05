356d6c75d26d355c8b0ad83d7629cc34567c743b
Pankaj Kumar <pankaj.gat@gmail.com>
2022-07-05 07:29:21 +0530

    Time: 10 ms (48.84%), Space: 45 MB (52.32%) - LeetHub

---
+++ 2053-kth-distinct-string-in-an-array/2053-kth-distinct-string-in-an-array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String kthDistinct(String[] arr, int k) {
+        HashMap<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if(map.getOrDefault(arr[i], 0) == 1){
+                if(--k == 0){
+                    return arr[i];
+                }
+            }
+        }
+        return "";
+    }
+}
