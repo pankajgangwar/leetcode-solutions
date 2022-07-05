ec814cad75aba398466e6a010cdee385d41457b6
Pankaj Kumar <pankaj.gat@gmail.com>
2022-07-05 08:20:38 +0530

    Time: 63 ms (44.44%), Space: 148.7 MB (57.78%) - LeetHub

--- 1983-widest-pair-of-indices-with-equal-range-sum/1983-widest-pair-of-indices-with-equal-range-sum.java
+++ 1983-widest-pair-of-indices-with-equal-range-sum/1983-widest-pair-of-indices-with-equal-range-sum.java
@@ -1,25 +1,20 @@
 class Solution {
     public int widestPairOfIndices(int[] nums1, int[] nums2) {
-       int n1 = nums1.length;
+        int n1 = nums1.length;
         int n2 = nums2.length;
-        int[] p1 = new int[n1 + 1];
-        int[] p2 = new int[n2 + 1];
-        for (int i = 1; i <= n1; i++) {
-            p1[i] = p1[i-1] + nums1[i-1];
-        }
-        for (int i = 1; i <= n2; i++) {
-            p2[i] = p2[i-1] + nums2[i-1];
-        }
         HashMap<Integer, Integer> map = new HashMap<>();
-        int max = 0;
-        for (int i = 0; i < p1.length; i++) {
-            int diff = p1[i] - p2[i];
+        map.put(0, -1);
+        int max = 0, sum1 = 0, sum2 = 0;
+        for (int i = 0; i < n1; i++) {
+            sum1 += nums1[i];
+            sum2 += nums2[i];
+            int diff = sum1 - sum2;
             if(map.containsKey(diff)){
                 max = Math.max(max, i - map.get(diff));
             }else{
                 map.put(diff, i);
             }
         }
-        return max; 
+        return max;
     }
 }
