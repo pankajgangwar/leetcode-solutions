72ea692a634d6ee1eff90379013f11d24060f367
Pankaj Kumar <pankaj.gat@gmail.com>
2022-07-11 09:41:29 +0530

    Time: 340 ms (10.00%), Space: 112.2 MB (10.00%) - LeetHub

---
+++ 2337-move-pieces-to-obtain-a-string/2337-move-pieces-to-obtain-a-string.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean canChange(String start, String target) {
+        int n = start.length();
+        if(!start.replaceAll("_", "").equals(target.replaceAll("_",""))){
+            return false;
+        }
+        for (int i = 0, j = 0; i < n && j < n ; i++,j++) {
+            while (i < n && start.charAt(i) == '_') i++;
+            while (j < n && target.charAt(j) == '_') j++;
+            if(i < n && start.charAt(i) == 'L' && i < j || j < n && start.charAt(i) == 'R' && i > j ){
+                return false;
+            }
+        }
+        return true;
+    }
+}
