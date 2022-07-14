d927860074a078da8ed822f35c92769c905744c3
Pankaj Kumar <pankaj.gat@gmail.com>
2022-07-14 07:21:37 +0530

    Time: 2456 ms (5.28%), Space: 89.7 MB (50.84%) - LeetHub

--- 2040-kth-smallest-product-of-two-sorted-arrays/2040-kth-smallest-product-of-two-sorted-arrays.java
+++ 2040-kth-smallest-product-of-two-sorted-arrays/2040-kth-smallest-product-of-two-sorted-arrays.java
@@ -1,57 +1,55 @@
 class Solution {
     public long kthSmallestProduct(int[] nums1, int[] nums2, long k) {
-        long lo = (long) -10e10, hi = (long) 10e10;
-        while (lo < hi) {
-            long mi = lo + (hi - lo) / 2;
-            if (check(nums1, nums2, mi) >= k) {
-                hi = mi;
-            } else {
-                lo = mi + 1;
+        long low = (long)-10e10, high = (long)10e10;
+        while (low < high){
+            long mid = low + (high - low) / 2;
+            if((check(nums1, nums2 , mid)) >= k){
+                high = mid;
+            }else{
+                low = mid + 1;
             }
         }
-        return lo;
+        return low;
     }
-    
+
     private long check(int[] nums1, int[] nums2, long target) {
         long res = 0;
-        for (int num : nums1) {
-            if (num < 0) {
-                // [num * nums2] is descending, 找num * nums2 <= target的个数
-                res += find1(nums2, (long) num, target);
-            } else if (num > 0) {
-                // [num * nums2] os ascending
-                res += find2(nums2, (long) num, target);
-            } else {
+        for(int n1 : nums1){
+            if(n1 < 0){
+                res += find1(nums2, (long)n1, target);
+            }else if(n1 > 0){
+                res += find2(nums2, (long)n1, target);
+            }else{
                 res += target >= 0 ? nums2.length : 0;
             }
         }
         return res;
     }
-    
+
     private int find1(int[] nums2, long n1, long target) {
-        int lo = 0, hi = nums2.length;
-        while (lo < hi) {
-            int mi = lo + (hi - lo) / 2;
-            if (n1 * nums2[mi] <= target) {
-                hi = mi;
-            } else {
-                lo = mi + 1;
+        int low = 0, high = nums2.length;
+        while (low < high){
+            int m = low + (high - low) / 2;
+            if(n1 * nums2[m] <= target){
+                high = m;
+            }else{
+                low = m + 1;
             }
         }
-        return nums2.length - lo;
+        return nums2.length - low;
     }
-    
+
     private int find2(int[] nums2, long n1, long target) {
-        int lo = 0, hi = nums2.length;
-        while (lo < hi) {
-            int mi = lo + (hi - lo) / 2;
-            if (n1 * nums2[mi] > target) {
-                hi = mi;
-            } else {
-                lo = mi + 1;
+        int low = 0, high = nums2.length;
+        while (low < high){
+            int m = low + (high - low) / 2;
+            if(n1 * nums2[m] > target){
+                high = m;
+            }else{
+                low = m + 1;
             }
         }
-        return lo;
+        return low;
     }
 
 }
