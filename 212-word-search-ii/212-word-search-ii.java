04f4789a4f684d65ff52795487b9cdde5ac82643
Pankaj Kumar <pankaj.gat@gmail.com>
2022-07-15 13:02:28 +0530

    Time: 182 ms (70.98%), Space: 43.7 MB (82.60%) - LeetHub

--- 212-word-search-ii/212-word-search-ii.java
+++ 212-word-search-ii/212-word-search-ii.java
@@ -1,124 +1,48 @@
 class Solution {
-    Set<String> found_words = new HashSet<>();
     public List<String> findWords(char[][] board, String[] words) {
-        Trie trie = new Trie();
-        for (String word : words) {
-            trie.insert(word);
-        }
-
-        int rows = board.length;
-        int cols = board[0].length;
-        boolean[][] visited = new boolean[rows][cols];
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                dfsTrie(board, i, j, "", trie, visited);
-            }
-        }
-
-        return new ArrayList<String>(found_words);
-    }
-    
-   private void dfsTrie(char[][] board, int i, int j, String str, Trie trie, boolean[][] visited) {
-        int rows = board.length;
-        int cols = board[0].length;
-
-        if(i < 0 || i >= rows || j < 0 || j >= cols || visited[i][j])
-            return;
-
-        str = str + board[i][j];
-        if(!trie.startsWith(str)){
-            return;
-        }
-        if(trie.search(str)){
-            found_words.add(str);
-        }
-        visited[i][j] = true;
-
-        dfsTrie(board, i+1, j, str, trie, visited);
-        dfsTrie(board, i, j+1, str, trie, visited);
-        dfsTrie(board, i - 1, j, str, trie, visited);
-        dfsTrie(board, i, j -1, str, trie, visited);
-
-        visited[i][j] = false;
-    }
-
-    class TrieNode {
-        public TrieNode[] children = new TrieNode[26];
-        String item = "";
-    }
-    class Trie {
-        public TrieNode root = new TrieNode();
-
-        public void insert(String word){
-            TrieNode node = root;
-            for (char ch : word.toCharArray()) {
-                if(node.children[ch - 'a'] == null){
-                    node.children[ch - 'a'] = new TrieNode();
-                }
-                node = node.children[ch - 'a'];
-            }
-            node.item = word;
-        }
-
-        public boolean search(String word){
-            TrieNode node = root;
-            for (char ch : word.toCharArray()) {
-                if(node.children[ch - 'a'] == null)
-                    return false;
-
-                node = node.children[ch - 'a'];
-            }
-            if(node.item.equals(word)){
-                return true;
-            }else{
-                return false;
-            }
-        }
-
-        public boolean startsWith(String prefix){
-            TrieNode node = root;
-            for (char ch  : prefix.toCharArray()) {
-                if(node.children[ch - 'a'] == null)
-                    return false;
-                node = node.children[ch - 'a'];
-            }
-            return true;
+    List<String> res = new ArrayList<>();
+    TrieNode root = buildTrie(words);
+    for (int i = 0; i < board.length; i++) {
+        for (int j = 0; j < board[0].length; j++) {
+            dfs (board, i, j, root, res);
         }
     }
-    
-     public boolean exist(char[][] board, String word) {
-        int rows = board.length;
-        int cols = board[0].length;
+    return res;
+}
 
-        boolean [][]visited = new boolean[rows][cols];
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if(anotherWordDFS(board, word, i, j, 0,visited)){
-                    return true;
-                }
-            }
-        }
-        return false;
+public void dfs(char[][] board, int i, int j, TrieNode p, List<String> res) {
+    char c = board[i][j];
+    if (c == '#' || p.next[c - 'a'] == null) return;
+    p = p.next[c - 'a'];
+    if (p.word != null) {   // found one
+        res.add(p.word);
+        p.word = null;     // de-duplicate
     }
-    
-     public boolean anotherWordDFS(char[][] board, String word, int curr_x, int curr_y, int curr_index, boolean[][] visited){
-        if(curr_index == word.length()){
-            return true;
-        }
-        if(curr_x < 0 || curr_x >= board.length || curr_y < 0 || curr_y >= board[0].length
-                || board[curr_x][curr_y] != word.charAt(curr_index) || visited[curr_x][curr_y]){
-            return false;
-        }
 
-        visited[curr_x][curr_y] = true;
+    board[i][j] = '#';
+    if (i > 0) dfs(board, i - 1, j ,p, res); 
+    if (j > 0) dfs(board, i, j - 1, p, res);
+    if (i < board.length - 1) dfs(board, i + 1, j, p, res); 
+    if (j < board[0].length - 1) dfs(board, i, j + 1, p, res); 
+    board[i][j] = c;
+}
 
-        boolean res = anotherWordDFS(board, word, curr_x + 1, curr_y, curr_index+1, visited) ||
-        anotherWordDFS(board, word, curr_x , curr_y + 1, curr_index+1, visited) ||
-        anotherWordDFS(board, word, curr_x - 1, curr_y, curr_index+1, visited) ||
-        anotherWordDFS(board, word, curr_x , curr_y -1, curr_index+1, visited);
-
-        visited[curr_x][curr_y] = false;
-
-        return res;
+public TrieNode buildTrie(String[] words) {
+    TrieNode root = new TrieNode();
+    for (String w : words) {
+        TrieNode p = root;
+        for (char c : w.toCharArray()) {
+            int i = c - 'a';
+            if (p.next[i] == null) p.next[i] = new TrieNode();
+            p = p.next[i];
+       }
+       p.word = w;
     }
+    return root;
+}
+
+class TrieNode {
+    TrieNode[] next = new TrieNode[26];
+    String word;
+}
 }
