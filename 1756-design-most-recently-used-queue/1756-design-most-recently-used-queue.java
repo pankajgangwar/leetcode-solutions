a2bc73ad0868ae9c9d7df355ec31ff1935088e6d
Pankaj Kumar <pankaj.gat@gmail.com>
2022-07-17 21:11:23 +0530

    Time: 111 ms (6.80%), Space: 54.3 MB (68.03%) - LeetHub

---
+++ 1756-design-most-recently-used-queue/1756-design-most-recently-used-queue.java
@@ -0,0 +1,23 @@
+class MRUQueue {
+
+    LinkedList<Integer> q = new LinkedList<>();
+        public MRUQueue(int n) {
+            for (int i = 1; i <= n; i++) {
+                q.add(i);
+            }
+        }
+
+        public int fetch(int k) {
+            k -= 1;
+            int last = q.get(k);
+            q.remove(k);
+            q.addLast(last);
+            return last;
+        }
+}
+
+/**
+ * Your MRUQueue object will be instantiated and called as such:
+ * MRUQueue obj = new MRUQueue(n);
+ * int param_1 = obj.fetch(k);
+ */
