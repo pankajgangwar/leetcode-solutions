575223d07ab1a9d635be1513598e5f8ae48b46d4
Pankaj Kumar <pankaj.gat@gmail.com>
2022-07-18 07:01:12 +0530

    Time: 74 ms (77.45%), Space: 41.5 MB (99.13%) - LeetHub

--- 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.java
+++ 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.java
@@ -1,26 +1,19 @@
 class Solution {
     public int getKth(int lo, int hi, int k) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[0] == b[0] ? -a[1] + b[1] : -a[0] + b[0]);
+        List<int[]> list = new ArrayList<>();
         for (int i = lo; i <= hi; i++) {
-            pq.offer(new int[]{getPower(i), i});
-            if(pq.size() > k){
-                pq.poll();
-            }
+            list.add(new int[]{getPower(i), i});
         }
-        return pq.peek()[1];
+        list.sort(Comparator.comparingInt(a -> a[0]));
+        return list.get(k-1)[1];
     }
 
-     HashMap<Integer, Integer> memo = new HashMap<>();
     public int getPower(int n){
-        if(n == 1 ) return n;
-        if(memo.containsKey(n)) return memo.get(n);
-        int res = 0;
+        if(n == 1 ) return 0;
         if(n % 2 == 0){
-             res = 1 + getPower(n / 2);
+            return 1 + getPower(n / 2);
         }else{
-            res = 1 + getPower(3 * n + 1);
+            return 1 + getPower(3 * n + 1);
         }
-        memo.put(n, res);
-        return memo.get(n);
     }
 }
