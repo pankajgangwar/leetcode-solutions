7f3f5de74cffeb7416c67e40840d318a6619feeb
Pankaj Kumar <pankaj.gat@gmail.com>
2022-07-19 12:55:05 +0530

    Time: 4 ms (85.38%), Space: 44.3 MB (90.12%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
@@ -9,15 +9,12 @@
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
         PriorityQueue<ListNode> mMinHeap = new PriorityQueue<ListNode>(new Comparator<ListNode>() {
-
 			@Override
 			public int compare(ListNode n1, ListNode n2) {
 				// TODO Auto-generated method stub
 				return n1.val - n2.val;
 			}
-			
 		});
-
 		ListNode head = new ListNode(0);
 		ListNode p = head;
 		for (ListNode n : lists) {
@@ -25,18 +22,14 @@ class Solution {
 				mMinHeap.add(n);
 			}
 		}
-        
-		
 		while (!mMinHeap.isEmpty()) {
 			ListNode current = mMinHeap.poll();
 			p.next = current;
 			p = p.next;
 			if(current.next != null) {
-                System.out.println(current.val);
 				mMinHeap.add(current.next);
 			}
 		}
-		
 		return head.next;
     }
 }
