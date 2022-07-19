35a68e040987c24f0ab49a79958a1ffa36ad8556
Pankaj Kumar <pankaj.gat@gmail.com>
2022-07-19 12:38:53 +0530

    Time: 23 ms (34.05%), Space: 48.6 MB (67.38%) - LeetHub

--- 2262-total-appeal-of-a-string/2262-total-appeal-of-a-string.java
+++ 2262-total-appeal-of-a-string/2262-total-appeal-of-a-string.java
@@ -6,11 +6,9 @@ class Solution {
         long res = 0;
         for (int i = 0; i < n; i++) {
             u[s.charAt(i) - 'a'] = i;
-            int[] v = Arrays.copyOf(u, 26);
-            //Arrays.sort(v);
             for (int j = 0; j < 26 ; j++) {
-                if(v[j] >= 0){
-                    res += v[j] + 1;
+                if(u[j] >= 0){
+                    res += u[j] + 1;
                 }
             }
         }
