bbe591b2124cfdfe84494b6cd1412efcf132ee3e
Pankaj Kumar <pankaj.gat@gmail.com>
2022-07-20 07:58:40 +0530

    Time: 1 ms (100.00%), Space: 42.1 MB (43.72%) - LeetHub

---
+++ 1723-find-minimum-time-to-finish-all-jobs/1723-find-minimum-time-to-finish-all-jobs.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int minimumTimeRequired(int[] jobs, int k) {
+        int[] workers = new int[k];
+        Arrays.sort(jobs);
+        int low = jobs[0];
+        int n = jobs.length;
+        int high = jobs[n - 1] * n;
+        while (low < high){
+            int mid = low + (high - low) / 2;
+            Arrays.fill(workers, mid);
+            if(dfs(jobs, workers, jobs.length - 1, mid)){
+                high = mid;
+            }else{
+                low = mid + 1;
+            }
+        }
+        return high;
+    }
+
+    public boolean dfs(int[] jobs, int[] workers, int j, int x){
+        if(j == -1) return true;
+        for (int i = 0; i < workers.length; i++) {
+            if(workers[i] >= jobs[j]){
+                workers[i] -= jobs[j];
+                if(dfs(jobs, workers, j - 1, x)){
+                    return true;
+                }
+                workers[i] += jobs[j];
+            }
+            if(workers[i] == x){
+                break;
+            }
+        }
+        return false;
+    }
+}
