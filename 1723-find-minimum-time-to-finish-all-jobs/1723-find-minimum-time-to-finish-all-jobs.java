cc7c11b33073c1457537cbb3de0306959330855c
Pankaj Kumar <pankaj.gat@gmail.com>
2022-07-20 08:24:26 +0530

    Time: 58 ms (60.00%), Space: 42 MB (47.43%) - LeetHub

--- 1723-find-minimum-time-to-finish-all-jobs/1723-find-minimum-time-to-finish-all-jobs.java
+++ 1723-find-minimum-time-to-finish-all-jobs/1723-find-minimum-time-to-finish-all-jobs.java
@@ -20,5 +20,40 @@ class Solution {
             if(workers[i] == 0) break;
         }
     }
+    
+    public int minimumTimeRequiredBinarySearch(int[] jobs, int k) {
+        int[] workers = new int[k];
+        Arrays.sort(jobs);
+        int low = jobs[0];
+        int n = jobs.length;
+        int high = (int)1e9;
+        while (low < high){
+            int mid = low + (high - low) / 2;
+            Arrays.fill(workers, mid);
+            if(search(jobs, workers, jobs.length - 1, mid)){
+                high = mid;
+            }else{
+                low = mid + 1;
+            }
+        }
+        return high;
+    }
+
+    public boolean search(int[] jobs, int[] workers, int j, int x){
+        if(j == -1) return true;
+        for (int i = 0; i < workers.length; i++) {
+            if(workers[i] >= jobs[j]){
+                workers[i] -= jobs[j];
+                if(search(jobs, workers, j - 1, x)){
+                    return true;
+                }
+                workers[i] += jobs[j];
+            }
+            if(workers[i] == x){
+                break;
+            }
+        }
+        return false;
+    }
 
 }
