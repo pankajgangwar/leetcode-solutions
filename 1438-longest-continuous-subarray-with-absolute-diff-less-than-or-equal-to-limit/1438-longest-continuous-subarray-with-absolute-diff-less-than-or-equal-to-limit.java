67c8b03fbe7c7a9d353c341a96eaf46a9668495c
Pankaj Kumar <pankaj.gat@gmail.com>
2022-07-21 08:33:49 +0530

    Time: 145 ms (21.77%), Space: 79.5 MB (11.31%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
@@ -1,5 +1,20 @@
 class Solution {
+    
     public int longestSubarray(int[] nums, int limit) {
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        int i = 0, j = 0;
+        for (; i < nums.length; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+            if(map.lastKey() - map.firstKey() > limit){
+                 map.put(nums[j], map.get(nums[j]) - 1);
+                if(map.get(nums[j]) == 0) map.remove(nums[j]);
+                j++;
+            }
+        }
+        return i - j;
+    }
+    
+    public int longestSubarrayDeque(int[] nums, int limit) {
         int start = 0;
         Deque<Integer> mindq = new ArrayDeque<>();
         Deque<Integer> maxdq = new ArrayDeque<>();
