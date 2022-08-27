cc315d496baf5542c717b68aa58f63fc54de2513
Pankaj Kumar <pankaj.gat@gmail.com>
2022-08-27 23:18:26 +0530

    Time: 16 ms (25.00%), Space: 124.4 MB (25.00%) - LeetHub

--- 2381-shifting-letters-ii/2381-shifting-letters-ii.java
+++ 2381-shifting-letters-ii/2381-shifting-letters-ii.java
@@ -21,15 +21,16 @@ class Solution {
         int n = s.length();
         int[] line = new int[n + 1];
         for (int i = 0; i < shifts.length; i++) {
-            /*line[shifts[i][0]] += (shifts[i][2] == 1) ? 1 : -1;
-            line[shifts[i][1] + 1] += (shifts[i][2] == 1) ? -1 : +1;*/
-            add(shifts[i][0], (shifts[i][2] == 1) ? 1 : -1);
-            add(shifts[i][1] + 1, (shifts[i][2] == 1) ? -1 : 1);
+            line[shifts[i][0]] += (shifts[i][2] == 1) ? 1 : -1;
+            line[shifts[i][1] + 1] += (shifts[i][2] == 1) ? -1 : +1;
+            //add(shifts[i][0], (shifts[i][2] == 1) ? 1 : -1);
+            //add(shifts[i][1] + 1, (shifts[i][2] == 1) ? -1 : 1);
         }
         char[] arr = s.toCharArray();
-        int count = 0;
-        for (int i = 0; i < n; i++) {
-            arr[i] = (char) ('a' + (26 + (arr[i] - 'a') + prefixSum(i) % 26) % 26);
+        for (int i = 0, count = 0; i < n; i++) {
+            //arr[i] = (char) ('a' + (26 + (arr[i] - 'a') + prefixSum(i) % 26) % 26);
+            count = (count + line[i]) % 26;
+            arr[i] = (char)('a' + (26 + (arr[i] - 'a') + count) % 26);
         }
         return new String(arr);
     }
