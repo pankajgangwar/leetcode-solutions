8766a3d1c798a5b0f34d6e0cfcc0d4194905c7f6
Pankaj Kumar <pankaj.gat@gmail.com>
2022-09-11 00:22:29 +0530

    Time: 236 ms (66.07%), Space: 42.1 MB (96.65%) - LeetHub

--- 2184-number-of-ways-to-build-sturdy-brick-wall/2184-number-of-ways-to-build-sturdy-brick-wall.java
+++ 2184-number-of-ways-to-build-sturdy-brick-wall/2184-number-of-ways-to-build-sturdy-brick-wall.java
@@ -1,41 +1,24 @@
 class Solution {
     public int buildWall(int height, int width, int[] bricks) {
-        List<Integer> validRows = new ArrayList<>();
-        buildRows(0, width, bricks, 0, validRows);
-        Map<Integer, Long> dp = new HashMap<>();
-        Map<Integer, List<Integer>> transitions = new HashMap<>();
-        for(Integer row : validRows){
-            transitions.put(row, new ArrayList<>());
-            dp.put(row, 1L);
-        }
-        for(Integer from : validRows){
-            for(Integer to : validRows){
-                if((from & to) == 0){
-                    transitions.get(from).add(to);
-                }
-            }
-        }
-        while (--height > 0){
-            Map<Integer, Long> nextDp = new HashMap<>();
-            for(Integer next : validRows){
-                long count = 0;
-                for(Integer prev : transitions.get(next)){
-                    count = (count + dp.get(prev)) % 1_000_000_007;
-                }
-                nextDp.put(next, count);
-            }
-            dp = nextDp;
-        }
-        return (int)(dp.values().stream().mapToLong(n -> n).sum() % 1_000_000_007);
+        return solve(height, width, bricks, new Integer[height + 1][1 << width], 0, 0, 0);
     }
 
-    void buildRows(int currWidth, int width, int[]bricks, int rowMask, List<Integer> r){
-        for(int brick : bricks){
-            if(currWidth + brick == width){
-                r.add(rowMask);
-            }else if(currWidth + brick < width){
-                buildRows(currWidth + brick, width, bricks, rowMask | (1 << (currWidth + brick)), r);
-            }
+    private int solve(int h, int w, int[] bricks, Integer[][]dp, int config, int curWidth, int prev){
+        if(dp[h][prev] != null){
+            return dp[h][prev];
         }
+        if(h == 0)return 1;
+        if(curWidth == w){
+            return solve(h - 1, w, bricks, dp, 0, 0, config);
+        }
+        int ans = 0;
+        for(int br : bricks){
+            int size = br + curWidth, nc = size < w ? config | (1 << size) : config;
+            if(size > w || (nc & prev) > 0 ){
+                continue;
+            }
+            ans = (ans + solve(h, w, bricks, dp, nc, size, prev)) % 1_000_000_007;
+        }
+        return config == 0 ? dp[h][prev] = ans : ans;
     }
 }
