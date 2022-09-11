7a89da7a2bff47b064cf506bddf3d2cf85931469
Pankaj Kumar <pankaj.gat@gmail.com>
2022-09-11 22:32:40 +0530

    Time: 19 ms (7.16%), Space: 43.3 MB (15.11%) - LeetHub

--- 1055-shortest-way-to-form-string/1055-shortest-way-to-form-string.java
+++ 1055-shortest-way-to-form-string/1055-shortest-way-to-form-string.java
@@ -1,5 +1,42 @@
 class Solution {
+    
     public int shortestWay(String source, String target) {
+        return shortestWayBFS(source, target);
+    }
+    
+    public int shortestWayBFS(String source, String target) {
+        for (int i = 0; i < target.length(); i++) {
+            String ch = target.charAt(i)+ "";
+            if(!source.contains(ch)){ // This character doesn't exists
+                return -1;
+            }
+        }
+        Queue<String> q = new LinkedList<>();
+        q.offer(target);
+        int min_steps = 0;
+        while (!q.isEmpty()){
+            String curr = q.poll();
+            int i = 0, j = 0;
+            while (i < curr.length() && j < source.length()){
+                if(curr.charAt(i) == source.charAt(j)){
+                    i++;
+                }
+                j++;
+            }
+            if(i < curr.length()){
+                String rem = curr.substring(i);
+                q.offer(rem);
+            }
+            if(i == curr.length()){
+                break;
+            }
+            min_steps++;
+        }
+        System.out.println("min_steps = " + min_steps);
+        return min_steps + 1;
+    }
+    
+    public int shortestWayBinarySearch(String source, String target) {
         ArrayList<Integer>[] list = new ArrayList[26];
         for (int i = 0; i < 26; i++) {
             list[i] = new ArrayList<>();
