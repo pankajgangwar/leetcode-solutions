75b5e9aac01e3a9083c76f89c0e2e87d43592b5f
Pankaj Kumar <pankaj.gat@gmail.com>
2022-12-11 09:55:26 +0530

    Time: 120 ms (16.67%), Space: 78.6 MB (83.33%) - LeetHub

--- 2501-longest-square-streak-in-an-array/2501-longest-square-streak-in-an-array.java
+++ 2501-longest-square-streak-in-an-array/2501-longest-square-streak-in-an-array.java
@@ -1,5 +1,35 @@
 class Solution {
+    
     public int longestSquareStreak(int[] nums) {
+        Arrays.sort(nums);
+        int[] a = uniq(nums);
+        int n = a.length;
+        int[]dp = new int[n];
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            int q = (int)Math.sqrt(a[i]);
+            dp[i] = 1;
+            if(q*q == a[i]){
+                int idx = Arrays.binarySearch(a, q);
+                if(idx >= 0){
+                    dp[i] = dp[idx] + 1;
+                }
+            }
+            ans = Math.max(ans, dp[i]);
+        }
+        return ans == 1 ? -1 : ans;
+    }
+
+    public int[] uniq(int[] nums){
+        if(nums.length == 0) return nums;
+        int p = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if(nums[i-1] != nums[i]) nums[p++] = nums[i];
+        }
+        return Arrays.copyOf(nums, p);
+    }
+    
+    public int longestSquareStreak1(int[] nums) {
         TreeSet<Integer> sets = new TreeSet<>();
         for (int i = 0; i < nums.length; i++) {
             sets.add(nums[i]);
