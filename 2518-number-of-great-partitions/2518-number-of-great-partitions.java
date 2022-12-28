4fcf9e04504e0498e0b2118e5f970b22579a3dcb
Pankaj Kumar <pankaj.gat@gmail.com>
2022-12-28 18:13:31 +0530

    Time: 5 ms (95.90%), Space: 41.5 MB (96.64%) - LeetHub

---
+++ 2518-number-of-great-partitions/2518-number-of-great-partitions.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int countPartitions(int[] nums, int k) {
+        int n = nums.length;
+        int[] dp = new int[k];
+        int mod = (int)1e9+7, ans = 1;
+        for (int i = 0; i < n; i++) {
+            ans = (ans*2)%mod;
+        }
+        dp[0] = 1;
+        long totalSum = 0;
+        for (int i = 0; i < n; i++) {
+            totalSum += nums[i];
+            for (int j = k - 1; j >= nums[i] ; j--) {
+                dp[j] = (dp[j] + dp[j - nums[i]]) % mod;
+            }
+        }
+        if(totalSum < 2L*k){
+            return 0;
+        }
+        int dpSum = 0;
+        for (int i = 0; i < k; i++) {
+            dpSum = (dpSum + dp[i]) % mod;
+        }
+        return (ans - 2*dpSum + mod) % mod;
+    }
+}
