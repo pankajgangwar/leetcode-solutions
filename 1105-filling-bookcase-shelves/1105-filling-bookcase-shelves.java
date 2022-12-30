a410601ac38ddbd7c47bb795ac32f5e2ae2c9932
Pankaj Kumar <pankaj.gat@gmail.com>
2022-12-30 11:37:15 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (69.98%) - LeetHub

---
+++ 1105-filling-bookcase-shelves/1105-filling-bookcase-shelves.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minHeightShelves(int[][] books, int shelfWidth) {
+        int n = books.length;
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+
+        for (int i = 1; i <= n; i++) {
+            int w = books[i-1][0];
+            int h = books[i-1][1];
+            dp[i] = dp[i - 1] + h;
+            for (int j = i-1; j > 0 && w + books[j-1][0] <= shelfWidth ; --j) {
+               h = Math.max(h, books[j-1][1]);
+               w += books[j-1][0];
+               dp[i] = Math.min(dp[i], dp[j-1] + h);
+            }
+        }
+        return dp[n];
+    }
+}
