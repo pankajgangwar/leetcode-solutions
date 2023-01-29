1121830bc9200366004b139b1dcce3d67e74711f
Pankaj Kumar <pankaj.gat@gmail.com>
2023-01-29 12:22:45 +0530

    Time: 139 ms (20.00%), Space: 58.7 MB (40.00%) - LeetHub

--- 2551-put-marbles-in-bags/2551-put-marbles-in-bags.java
+++ 2551-put-marbles-in-bags/2551-put-marbles-in-bags.java
@@ -3,8 +3,9 @@ class Solution {
         int n = weights.length;
         if(k == 1 || k == n) return 0;
         k -= 1;
+        n -= 1;
         List<Long> sum = new ArrayList<>();
-        for(int i = 0; i < n - 1; i++ ){
+        for(int i = 0; i < n; i++ ){
             sum.add((long) (weights[i]+weights[i+1]));
         }
         long max = 0;
@@ -12,7 +13,7 @@ class Solution {
         Collections.sort(sum);
         for(int i = 0; i < k; i++){
             min += sum.get(i);
-            max += sum.get(n - i - 1 - 1);
+            max += sum.get(n - i - 1);
         }
         return (max - min);
     }
