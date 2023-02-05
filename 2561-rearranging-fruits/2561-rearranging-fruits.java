bda1b062f889e80dcd5aaebb4230b1a659cb6913
Pankaj Kumar <pankaj.gat@gmail.com>
2023-02-05 13:06:18 +0530

    Time: 85 ms (25.00%), Space: 73.7 MB (25.00%) - LeetHub

--- 2561-rearranging-fruits/2561-rearranging-fruits.java
+++ 2561-rearranging-fruits/2561-rearranging-fruits.java
@@ -15,27 +15,27 @@ class Solution {
         }
         for (Map.Entry<Integer, Integer> e : freq.entrySet()) {
             mn = Math.min(mn, e.getKey());
-            if ((e.getValue() & 1) == 1) return -1;
+            if ((e.getValue() % 2) != 0) return -1;
         }
-        ArrayList<Integer> aa = new ArrayList<>();
-        ArrayList<Integer> bb = new ArrayList<>();
+        ArrayList<Integer> b1List = new ArrayList<>();
+        ArrayList<Integer> b2List = new ArrayList<>();
         for (Map.Entry<Integer, Integer> e : b1Freq.entrySet()) {
             while (e.getValue() > freq.get(e.getKey()) / 2) {
-                aa.add(e.getKey());
+                b1List.add(e.getKey());
                 e.setValue(e.getValue() - 1);
             }
         }
         for (Map.Entry<Integer, Integer> e : b2Freq.entrySet()) {
             while (e.getValue() > freq.get(e.getKey()) / 2) {
-                bb.add(e.getKey());
+                b2List.add(e.getKey());
                 e.setValue(e.getValue() - 1);
             }
         }
-        Collections.sort(aa);
-        Collections.sort(bb, Collections.reverseOrder());
+        Collections.sort(b1List);
+        Collections.sort(b2List, Collections.reverseOrder());
         long res = 0;
-        for (int i = 0; i < aa.size(); i++) {
-            res = res + Math.min(Math.min(aa.get(i), bb.get(i)), mn * 2);
+        for (int i = 0; i < b1List.size(); i++) {
+            res = res + Math.min(Math.min(b1List.get(i), b2List.get(i)), mn * 2);
         }
         return res;
     }
