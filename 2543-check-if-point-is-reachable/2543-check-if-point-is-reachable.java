6d7bd98770a2405573ed54ca9648abf5dcad6a24
Pankaj Kumar <pankaj.gat@gmail.com>
2023-02-09 09:03:38 +0530

    Time: 1 ms (22.32%), Space: 39.3 MB (64.26%) - LeetHub

--- 2543-check-if-point-is-reachable/2543-check-if-point-is-reachable.java
+++ 2543-check-if-point-is-reachable/2543-check-if-point-is-reachable.java
@@ -3,6 +3,7 @@ class Solution {
     public boolean isReachable(int x, int y) {
         int n = BigInteger.valueOf(x).gcd(BigInteger.valueOf(y)).intValue();
         //return (n & (n-1)) == 0;//Brian Kernighan’s algorithm
-        return (n == (n & -n)); // check lowest bit
+        //return (n == (n & -n)); // check lowest bit
+        return BigInteger.valueOf(n).bitCount() == 1;
     }
 }
