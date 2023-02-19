b86481152a94e7648f29f7f187a1a14aa765a975
Pankaj Kumar <pankaj.gat@gmail.com>
2023-02-19 13:42:45 +0530

    Time: 0 ms (100.00%), Space: 39.4 MB (50.00%) - LeetHub

--- 2571-minimum-operations-to-reduce-an-integer-to-0/2571-minimum-operations-to-reduce-an-integer-to-0.java
+++ 2571-minimum-operations-to-reduce-an-integer-to-0/2571-minimum-operations-to-reduce-an-integer-to-0.java
@@ -1,9 +1,26 @@
 class Solution {
+    
     public int minOperations(int n) {
+        return f(n);
+    }
+
+    HashMap<Integer, Integer> map = new HashMap<>();
+    public int f(int n) {
+        if(map.containsKey(n)) return map.get(n);
+        if(n == 0) return 0;
+        if((n & (n - 1)) == 0) return 1;
+        int x = n & -n;
+        int res = 1 + Math.min(f(n - x), f(n + x));
+        map.put(n, res);
+        return res;
+    }
+    
+    public int minOperations1(int n) {
         int pow = (int)(Math.log(n) / Math.log(2));
         if((1 << pow) == n) return 1;
         int diff1 = minOperations(n - (1 << pow));
         int diff2 = minOperations((1 << (pow + 1)) - n);
         return 1 + Math.min(diff1, diff2);
     }
+    
 }
