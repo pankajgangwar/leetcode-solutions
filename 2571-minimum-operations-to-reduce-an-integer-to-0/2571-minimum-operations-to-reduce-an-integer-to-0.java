9566287744b36530a18a1aa9a0378e0092ad40d5
Pankaj Kumar <pankaj.gat@gmail.com>
2023-02-19 14:33:24 +0530

    Time: 0 ms (100.00%), Space: 38.8 MB (100.00%) - LeetHub

--- 2571-minimum-operations-to-reduce-an-integer-to-0/2571-minimum-operations-to-reduce-an-integer-to-0.java
+++ 2571-minimum-operations-to-reduce-an-integer-to-0/2571-minimum-operations-to-reduce-an-integer-to-0.java
@@ -8,8 +8,8 @@ class Solution {
     public int f(int n) {
         if(map.containsKey(n)) return map.get(n);
         if(n == 0) return 0;
-        if((n & (n - 1)) == 0) return 1;
-        int x = n & -n;
+        if((n & (n - 1)) == 0) return 1; // if n is power of 2
+        int x = n & -n; // smallest power of 2 i.e lsb of n
         int res = 1 + Math.min(f(n - x), f(n + x));
         map.put(n, res);
         return res;
@@ -17,8 +17,7 @@ class Solution {
     
     public int minOperations(int n) {
         int pow = (int)(Math.log(n) / Math.log(2));
-        //if((1 << pow) == n) return 1;
-        if((n & (n - 1)) == 0) return 1;
+        if((1 << pow) == n) return 1;
         int op1 = minOperations(n - (1 << pow));
         int op2 = minOperations((1 << (pow + 1)) - n);
         return 1 + Math.min(op1, op2);
