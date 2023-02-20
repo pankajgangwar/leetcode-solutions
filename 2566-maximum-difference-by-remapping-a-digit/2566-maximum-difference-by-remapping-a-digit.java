839a2861373b98ffee8d38cf17dc0729f93f4a75
Pankaj Kumar <pankaj.gat@gmail.com>
2023-02-20 07:48:16 +0530

    Time: 10 ms (14.29%), Space: 41.6 MB (14.29%) - LeetHub

--- 2566-maximum-difference-by-remapping-a-digit/2566-maximum-difference-by-remapping-a-digit.java
+++ 2566-maximum-difference-by-remapping-a-digit/2566-maximum-difference-by-remapping-a-digit.java
@@ -1,5 +1,35 @@
 class Solution {
+    
+    public int getNumber(int n, int d_from, int d_to){
+        List<Integer> digits = new ArrayList<>();
+        while(n > 0){
+            digits.add(n % 10);
+            n /= 10;
+        }
+        Collections.reverse(digits);
+        int ret = 0;
+        for(int d : digits){
+            if(d == d_from){
+                d = d_to;
+            }
+            ret = d + ret * 10;
+        }
+        return ret;
+    }
+
     public int minMaxDifference(int num) {
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for(int d1 = 0; d1 <= 9; d1++){
+            for (int d2 = 0; d2 <= 9 ; d2++) {
+                min = Math.min(min, getNumber(num, d1, d2));
+                max = Math.max(max, getNumber(num, d1, d2));
+            }
+        }
+        return max - min;
+    }
+    
+    public int minMaxDifference1(int num) {
         String s = String.valueOf(num);
         StringBuilder maxStr = new StringBuilder(s);
         for (int i = 0; i < s.length(); i++) {
