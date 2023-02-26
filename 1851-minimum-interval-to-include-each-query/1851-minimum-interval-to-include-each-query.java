71531d7235f246f67dcf265d7d772ed2f5f51f25
Pankaj Kumar <pankaj.gat@gmail.com>
2023-02-26 12:07:37 +0530

    Time: 146 ms (41.28%), Space: 82.5 MB (98.21%) - LeetHub

--- 1851-minimum-interval-to-include-each-query/1851-minimum-interval-to-include-each-query.java
+++ 1851-minimum-interval-to-include-each-query/1851-minimum-interval-to-include-each-query.java
@@ -2,18 +2,18 @@ class Solution {
     public int[] minInterval(int[][] intervals, int[] queries) {
        TreeMap<Integer, Integer> map = new TreeMap<>();
         Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
-        int[] q = queries.clone();
-        Arrays.sort(q);
+        int[] sortedQueries = queries.clone();
+        Arrays.sort(sortedQueries);
         HashMap<Integer, Integer> hashMap = new HashMap<>();
-        for (int i = 0, j = 0; i < q.length ; i++) {
-            while (j < intervals.length && intervals[j][0] <= q[i]){
+        for (int i = 0, j = 0; i < sortedQueries.length; i++) {
+            while (j < intervals.length && intervals[j][0] <= sortedQueries[i]){
                 map.put(intervals[j][1] - intervals[j][0] + 1, intervals[j][1]);
                 j++;
             }
-            while (!map.isEmpty() && map.firstEntry().getValue() < q[i]){
+            while (!map.isEmpty() && map.firstEntry().getValue() < sortedQueries[i]){
                 map.pollFirstEntry();
             }
-            hashMap.put(q[i], map.isEmpty() ? -1 : map.firstKey());
+            hashMap.put(sortedQueries[i], map.isEmpty() ? -1 : map.firstKey());
         }
         int[] ans = new int[queries.length];
         for (int i = 0; i < queries.length; i++) {
