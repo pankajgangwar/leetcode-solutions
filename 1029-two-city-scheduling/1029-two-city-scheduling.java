59cd8cf0b038d8bd23d7b068c7aed73ec04c77b0
Pankaj Kumar <pankaj.gat@gmail.com>
2023-03-12 12:45:55 +0530

    Time: 11 ms (5.01%), Space: 42.4 MB (11.00%) - LeetHub

---
+++ 1029-two-city-scheduling/1029-two-city-scheduling.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int twoCitySchedCost(int[][] costs) {
+      int n = costs.length;
+		int totalCost = 0;
+		
+		Arrays.sort(costs, new Comparator<int[]>() {
+			@Override
+			public int compare(int[] a, int[] b) {
+                int diffa = a[0] - a[1];
+                int diffb = b[0] - b[1];
+                return Integer.compare(diffa, diffb);
+			}
+		});
+        
+        for(int[] c : costs){
+            System.out.println(Arrays.toString(c));
+        }
+
+		for (int i = 0; i < n/2; i++) {
+			totalCost+= costs[i][0];
+		}
+		
+		for (int i = n/2; i < n; i++) {
+			totalCost+= costs[i][1];
+		}
+
+		return totalCost;
+	  
+    }
+}
