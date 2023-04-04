7ff5506af4f5e5b1021be924a29bf64744e49f55
Pankaj Kumar <pankaj.gat@gmail.com>
2023-04-04 14:07:57 +0530

    Time: 1001 ms (5.06%), Space: 45.9 MB (6.07%) - LeetHub

---
+++ 2608-shortest-cycle-in-a-graph/2608-shortest-cycle-in-a-graph.java
@@ -0,0 +1,74 @@
+class Solution {
+    public int findShortestCycle(int n, int[][] edges) {
+        HashMap<Integer, HashSet<Integer>> graph = new HashMap<>();
+        for(int i = 0; i < n; i++) graph.putIfAbsent(i, new HashSet<>());
+        for(int[] e : edges){
+            int src = e[0];
+            int dst = e[1];
+            graph.get(src).add(dst);
+            graph.get(dst).add(src);
+        }
+        int ans = Integer.MAX_VALUE;
+        for(int i = 0; i < n; i++){
+            ArrayDeque<Integer> q = new ArrayDeque<>();
+            int src = i;
+            q.offer(src);
+
+            int[] distance = new int[n];
+            int[] parent = new int[n];
+
+            Arrays.fill(distance, Integer.MAX_VALUE);
+            Arrays.fill(parent, -1);
+
+            distance[src] = 0;
+            while (!q.isEmpty()){
+                int size = q.size();
+                while (size-- > 0){
+                    int par = q.poll();
+                    for(int child : graph.get(par)){
+                        if(distance[child] == Integer.MAX_VALUE){
+                            distance[child] = distance[par] + 1;
+                            q.offer(child);
+                            parent[child] = par;
+                        }else if(parent[par] != child && parent[child] != par){
+                            ans = Math.min(ans, distance[par] + distance[child] + 1);
+                        }
+                    }
+                }
+            }
+        }
+        return ans == Integer.MAX_VALUE ? -1 : ans;
+    }
+
+    public int bfs(HashMap<Integer, HashSet<Integer>> graph, int src, int n){
+        ArrayDeque<Integer> q = new ArrayDeque<>();
+        q.offer(src);
+
+        int[] distance = new int[n];
+        int[] parent = new int[n];
+
+        Arrays.fill(distance, Integer.MAX_VALUE);
+        Arrays.fill(parent, -1);
+
+        distance[src] = 0;
+        int inf = Integer.MAX_VALUE;;
+        int ans = inf;
+        while (!q.isEmpty()){
+            int size = q.size();
+            while (size-- > 0){
+                int par = q.poll();
+                for(int child : graph.get(par)){
+                    if(distance[child] == Integer.MAX_VALUE){
+                        distance[child] = distance[par] + 1;
+                        q.offer(child);
+                        parent[child] = par;
+                    }else if(parent[par] != child && parent[child] != par){
+                        //ans = Math.min(ans, distance[par] + distance[child] + 1);
+                        return distance[par] + distance[child] + 1;
+                    }
+                }
+            }
+        }
+        return Integer.MAX_VALUE;
+    }
+}
