34041c958f2507c928f32940500e593d4f9e4cde
Pankaj Kumar <pankaj.gat@gmail.com>
2023-04-06 09:47:19 +0530

    Time: 30 ms (93.94%), Space: 44.2 MB (45.89%) - LeetHub

--- 1797-design-authentication-manager/1797-design-authentication-manager.java
+++ 1797-design-authentication-manager/1797-design-authentication-manager.java
@@ -15,9 +15,10 @@ class AuthenticationManager {
 
         public void renew(String tokenId, int currentTime) {
             if(map.containsKey(tokenId)){
-                int expireTime = map.get(tokenId);
+                Integer expireTime = map.get(tokenId);
                 if(expireTime > currentTime){
                     map.put(tokenId, currentTime + time);
+                    tokens.remove(expireTime);
                     tokens.add((currentTime + time));
                 }else{
                     map.remove(tokenId);
@@ -26,8 +27,17 @@ class AuthenticationManager {
         }
 
         public int countUnexpiredTokens(int currentTime) {
-            map.entrySet().removeIf(e -> e.getValue() <= currentTime);
-            return map.size();
+            /*map.entrySet().removeIf(e -> e.getValue() <= currentTime);
+            return map.size();*/
+            
+            //System.out.println("CurrTime " + currentTime +  " tokens " + tokens.toString());
+            int idx = Collections.binarySearch(tokens, currentTime);
+            if(idx < 0){
+                idx = ~idx;
+                return tokens.size() - idx;
+            }else{
+                return tokens.size() - idx - 1;
+            }
         }
 }
 
