52c3dc7864962fd93abd45e0f7b851b1cc795ac0
Pankaj Kumar <pankaj.gat@gmail.com>
2023-04-08 13:47:32 +0530

    Time: 181 ms (22.73%), Space: 110.9 MB (59.09%) - LeetHub

--- 1778-shortest-path-in-a-hidden-grid/1778-shortest-path-in-a-hidden-grid.java
+++ 1778-shortest-path-in-a-hidden-grid/1778-shortest-path-in-a-hidden-grid.java
@@ -9,88 +9,73 @@
  */
 
 class Solution {
-    private static final int BLOCKED = -1;
-    private static final int UNEXPLORED = 0;
-    private static final int PATH = 1;
-    private static final int TARGET = 2;
-    private static final int START = 3;
-        
+    int[][] dxys = new int[][] {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    char[] dirs = new char[] {'U', 'D', 'L', 'R'};
+    char[] rev_dirs = new char[] {'D', 'U', 'R', 'L'};
+
+    int[] target = new int[]{-100, -100};
+    int m = 501;
+    int s = m - 1;
     public int findShortestPath(GridMaster master) {
-        int m = 1002;
-        int[][] board = new int[m][m];
-        int[] start = new int[] {500, 500};
-        board[500][500] = START;
-        int[] target = findTarget(master, board, 500, 500);
-        return target == null ? -1 : getDistance(board, target, start);
-    }
-    
-	// dfs
-    private int[] findTarget(GridMaster master, int[][] board, int r, int c) {
-        if (master.isTarget()) {
-            board[r][c] = TARGET;
-            return new int[]{r,c};
+        int[][] grid = new int[m*2][m*2];
+        for (int i = 0; i < (m*2); i++) {
+            Arrays.fill(grid[i], -1);
         }
-            
-        int[] res = null;
-        for (Direction d : Direction.values()) {
-            int r2 = r + d.n[0];
-            int c2 = c + d.n[1];
-            if (board[r2][c2] == UNEXPLORED) {
-                if (master.canMove(d.c)) {
-                    master.move(d.c);
-                    board[r2][c2] = PATH;
-                    int[] target = findTarget(master, board, r2, c2);
-                    if (target != null)
-                        res = target;
-                    master.move(d.o);
-                } else {
-                    board[r2][c2] = BLOCKED;
+        grid[s][s] = 0;
+        fillGrid(s, s, grid, master);
+        boolean[][] visited = new boolean[m*2][m*2];
+
+        Queue<int[]> pq = new LinkedList<>();
+        pq.offer(new int[]{s, s});
+
+        int steps = 0;
+        while (!pq.isEmpty()){
+            int size = pq.size();
+            while(size-- > 0){
+                int[] curr = pq.poll();
+                int curr_x = curr[0], curr_y = curr[1];
+                if(curr_x == target[0] && curr_y == target[1]){
+                    return steps;
                 }
-            }
-        }
-        return res;
-    }
-    
-	// bfs
-    private int getDistance(int[][] board, int[] target, int[] start) {
-        Queue<int[]> queue = new ArrayDeque<>();
-        queue.add(target);
-        int distance = 1;
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            for (int i = 0; i < size; i++) {
-                int[] cur = queue.poll();    
-                for (Direction d : Direction.values()) {
-                    int r2 = cur[0] + d.n[0];
-                    int c2 = cur[1] + d.n[1];
-                    if (board[r2][c2] == START)
-                        return distance;
-                    else if (board[r2][c2] == PATH) {
-                        board[r2][c2] = BLOCKED;
-                        queue.add(new int[]{r2,c2});
+                if(visited[curr_x][curr_y]) continue;
+                visited[curr_x][curr_y] = true;
+                for (int i = 0; i < dxys.length; i++) {
+                    int next_x = dxys[i][0] + curr_x;
+                    int next_y = dxys[i][1] + curr_y;
+                    if(next_x < 0 || next_x >= grid.length
+                            || next_y < 0 || next_y >= grid[0].length
+                            || visited[next_x][next_y] || grid[next_x][next_y] == -1){
+                        continue;
                     }
+                    pq.offer(new int[]{next_x, next_y});
                 }
             }
-            distance++;            
+            steps++;
         }
-        
-        return distance;
-    }    
-    
-    enum Direction {
-        UP('U', 'D', new int[]{0,1}),
-        DOWN('D', 'U', new int[]{0,-1}),
-        LEFT('L', 'R', new int[]{-1,0}),
-        RIGHT('R', 'L', new int[]{1,0});
-        
-        public char c;
-        public char o;
-        public int[] n;
-        
-        Direction(char c, char o, int[] n) {
-            this.c = c;
-            this.o = o;
-            this.n = n;
-        }            
+        return -1;
+    }
+
+    private void fillGrid(int currX, int currY,
+                          int[][] grid, GridMaster master) {
+        if(master.isTarget()){
+            target[0] = currX;
+            target[1] = currY;
+        }
+
+        for (int i = 0; i < dirs.length; i++) {
+            char ch = dirs[i];
+            char revCh = rev_dirs[i];
+            int next_x = currX + dxys[i][0];
+            int next_y = currY + dxys[i][1];
+            if(next_x >= grid.length || next_x < 0 || next_y < 0 || next_y >= grid[0].length){
+                continue;
+            }
+            if(master.canMove(ch) && grid[next_x][next_y] == -1){
+                master.move(ch);
+                grid[next_x][next_y] = 1;
+                fillGrid(next_x, next_y, grid, master);
+                master.move(revCh);
+            }
+        }
     }
 }
