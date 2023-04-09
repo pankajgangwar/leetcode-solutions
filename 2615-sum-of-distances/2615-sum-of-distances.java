e8dc05b22cd322381851fd5713b988358e1e107f
Pankaj Kumar <pankaj.gat@gmail.com>
2023-04-09 12:23:01 +0530

    Time: 33 ms (66.67%), Space: 69.8 MB (33.33%) - LeetHub

--- 2615-sum-of-distances/2615-sum-of-distances.java
+++ 2615-sum-of-distances/2615-sum-of-distances.java
@@ -1,38 +1,29 @@
 class Solution {
     public long[] distance(int[] nums) {
+        HashMap<Integer, List<Integer>> map = new HashMap<>();
         int n = nums.length;
-        long[] ans = new long[n];
-        
-        Map<Integer, List<Integer>> map = new HashMap<>();
-        
-        // group indices by number value
         for (int i = 0; i < n; i++) {
-            int num = nums[i];
-            if (!map.containsKey(num)) {
-                map.put(num, new ArrayList<>());
-            }
-            map.get(num).add(i);
+            map.putIfAbsent(nums[i], new ArrayList<>());
+            map.get(nums[i]).add(i);
         }
-        
-
-        for (List<Integer> indices : map.values()) {
+        long[] arr = new long[n];
+        for(List<Integer> indices : map.values()){
             int size = indices.size();
-            if (size == 1) {
-                ans[indices.get(0)] = 0;
-            } else {
+            if(size == 1){
+                arr[indices.get(0)] = 0;
+            }else{
                 long nextSum = indices.stream().mapToLong(i -> i).sum();
                 long prevSum = 0;
-                for (int i = 0; i < size; i++) {
+                for(int i = 0; i < size; i++){
                     int index = indices.get(i);
                     long tmp = nextSum - ((long) (size - i) * index);
                     tmp += Math.abs(prevSum - (long) i * index);
-                    ans[index] = tmp;
+                    arr[index] = tmp;
                     nextSum -= index;
                     prevSum += index;
                 }
             }
         }
-        
-        return ans;
+        return arr;
     }
 }
