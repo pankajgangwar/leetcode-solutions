e6a46be66f4165aef62270a2cd78d10976c0a341
Pankaj Kumar <pankaj.gat@gmail.com>
2023-04-09 10:38:59 +0530

    Time: 940 ms (20.00%), Space: 57.9 MB (20.00%) - LeetHub

--- 2614-prime-in-diagonal/2614-prime-in-diagonal.java
+++ 2614-prime-in-diagonal/2614-prime-in-diagonal.java
@@ -1,6 +1,41 @@
 class Solution {
     
+    boolean[] sieve = new boolean[4000001];
+    public void buildSieve(){
+        sieve[0] = sieve[1] = true;
+        for (int p = 2; p*p <= 4000001 ; p++) {
+            if(!sieve[p]){
+                for (int i = p*p; i < 4000001; i += p) {
+                    sieve[i] = true;
+                }
+            }
+        }
+    }
+    
     public int diagonalPrime(int[][] nums) {
+        int n = nums.length;
+        int max = 0;
+        buildSieve();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if(i == j){
+                    int diag = nums[i][i];
+                    if(!sieve[diag]){
+                        max = Math.max(max, diag);
+                    }
+                }
+                if(j == (n - i - 1)){
+                    int antiDiag = nums[i][n - i - 1];
+                    if(!sieve[antiDiag]){
+                        max = Math.max(max, antiDiag);
+                    }
+                }
+            }
+        }
+        return max;
+    }
+    
+    public int diagonalPrime1(int[][] nums) {
         int n = nums.length;
         int max = 0;
         for (int i = 0; i < n; i++) {
