6b83ff34222414bf494f62875f9bd61d03205143
Pankaj Kumar <pankaj.gat@gmail.com>
2023-04-30 13:03:05 +0530

    Time: 16 ms (100.00%), Space: 43.5 MB (100.00%) - LeetHub

---
+++ 2663-lexicographically-smallest-beautiful-string/2663-lexicographically-smallest-beautiful-string.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String smallestBeautifulString(String s, int k) {
+        char[] arr = s.toCharArray();
+        for (int i = arr.length - 1; i >=0 ; i--) {
+            arr[i]++;
+            while (!isValid(arr, i)){
+                arr[i]++;
+            }
+            if(arr[i] < 'a' + k){
+                for(i = i + 1; i < s.length(); i++){
+                    for(arr[i] = 'a'; !isValid(arr, i); arr[i]++);
+                }
+                return new String(arr);
+            }
+        }
+        return "";
+    }
+
+    public boolean isValid(char[] arr, int i){
+        return (i < 1 || arr[i] != arr[i-1]) && (i < 2 || arr[i] != arr[i-2]);
+    }
+}
