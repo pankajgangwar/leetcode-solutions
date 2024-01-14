f4a0fcf81b26ee07061636e887d6d904e4bb8201
Pankaj Kumar <pankaj.gat@gmail.com>
2024-01-14 19:01:14 +0530

    Time: 7 ms (70.95%), Space: 44.7 MB (48.65%) - LeetHub

--- 1062-longest-repeating-substring/1062-longest-repeating-substring.java
+++ 1062-longest-repeating-substring/1062-longest-repeating-substring.java
@@ -64,20 +64,20 @@ class Solution {
         int right = n;
         while (left <= right){
             int mid = left + (right - left) / 2;
-            if(search(s, mid) != -1) left = mid + 1;
+            if(search(s, mid)) left = mid + 1;
             else right = mid - 1;
         }
         return left - 1;
     }
 
-    private int search(String s, int mid) {
+    private boolean search(String s, int mid) {
         int n = s.length();
         HashSet<Long> seen = new HashSet<>();
         for (int i = 0; i < n - mid + 1; i++) {
             long hash = s.substring(i, i + mid).hashCode();
-            if(!seen.add(hash)) return i;
+            if(!seen.add(hash)) return true;
         }
-        return -1;
+        return false;
     }
     
     //https://cp-algorithms.com/string/suffix-array.html
