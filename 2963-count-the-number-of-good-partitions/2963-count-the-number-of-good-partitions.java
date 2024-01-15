23ddb42d3ac5762fe907431acb8524de508729b0
Pankaj Kumar <pankaj.gat@gmail.com>
2024-01-15 14:55:57 +0530

    Time: 30 ms (79.83%), Space: 63.9 MB (31.40%) - LeetHub

--- 2963-count-the-number-of-good-partitions/2963-count-the-number-of-good-partitions.java
+++ 2963-count-the-number-of-good-partitions/2963-count-the-number-of-good-partitions.java
@@ -1,7 +1,7 @@
 class Solution {
     public int numberOfGoodPartitions(int[] A) {
         int res = 1, n = A.length, mod = (int)1e9+7;
-        Map<Integer, Integer> map = new TreeMap<>();
+        Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < n; i++) {
             map.put(A[i], i);
         }
