4ece6f558cab80b0313bb3e8bac462f0e26944e8
Pankaj Kumar <pankaj.gat@gmail.com>
2024-01-16 20:12:58 +0530

    Time: 13 ms (39.37%), Space: 56.4 MB (29.61%) - LeetHub

--- 2863-maximum-length-of-semi-decreasing-subarrays/2863-maximum-length-of-semi-decreasing-subarrays.java
+++ 2863-maximum-length-of-semi-decreasing-subarrays/2863-maximum-length-of-semi-decreasing-subarrays.java
@@ -1,5 +1,20 @@
 class Solution {
-    public int maxSubarrayLength(int[] nums) {
+    
+    public int maxSubarrayLength(int[] nums){
+        Stack<Integer> st = new Stack<>();
+        for (int i = 0; i < nums.length; i++) {
+            if(st.isEmpty() || nums[st.peek()] < nums[i]) st.push(i);
+        }
+        int res = 0;
+        for (int i = nums.length - 1; i >=0; i--) {
+            while (!st.isEmpty() && nums[st.peek()] > nums[i]){
+                res = Math.max(res, i - st.pop() + 1);
+            }
+        }
+        return res;
+    }
+    
+    public int maxSubarrayLength1(int[] nums) {
         TreeMap<Integer, Integer> map = new TreeMap<>();
         int n = nums.length;
         int res = 0;
@@ -10,6 +25,7 @@ class Solution {
                     map.put(nums[i], i);
                 }
             }else{
+                System.out.println("ele " + nums[i] + " higher " + higher);
                 res = Math.max(res, i - map.get(higher) + 1);
             }
         }
