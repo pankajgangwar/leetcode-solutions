63bf4c0b7479e78473112839c79390d2d44ec2a3
Pankaj Kumar <pankaj.gat@gmail.com>
2024-01-18 07:49:45 +0530

    Time: 24 ms (64.73%), Space: 45.9 MB (79.87%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -6,9 +6,7 @@ class Solution {
         prefixSumMap.put(0,1);
         for(int i = 0; i < nums.length; i++){
             sum += nums[i];
-            if(prefixSumMap.containsKey(sum - target)){
-                count += prefixSumMap.get(sum - target);
-            }
+            count += prefixSumMap.getOrDefault(sum - target, 0);
             prefixSumMap.put(sum, prefixSumMap.getOrDefault(sum, 0)  +1);
         }
         return count;
