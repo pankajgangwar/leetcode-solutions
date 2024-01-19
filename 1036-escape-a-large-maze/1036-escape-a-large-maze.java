1518037996933ae18afb911be9a1dcf2d4f8ec35
Pankaj Kumar <pankaj.gat@gmail.com>
2024-01-19 08:50:12 +0530

    Time: 203 ms (69.76%), Space: 62.2 MB (17.07%) - LeetHub

--- 1036-escape-a-large-maze/1036-escape-a-large-maze.java
+++ 1036-escape-a-large-maze/1036-escape-a-large-maze.java
@@ -1,21 +1,22 @@
 class Solution {
     
-    public boolean isEscapePossible(int[][] blocked, int[] source, int[] target) {
-    Set<Long> blockedSet = new HashSet<>();
+    public boolean isEscapePossible(int[][] blocked, int[] s, int[] t) {
+    Set<String> blockedSet = new HashSet<>();
     for (int[] b : blocked)
-      blockedSet.add(hash(b[0], b[1]));
+      blockedSet.add(b[0] + "," + b[1]);
 
-    return dfs(blockedSet, source[0], source[1], hash(target[0], target[1]), new HashSet<>()) &&
-        dfs(blockedSet, target[0], target[1], hash(source[0], source[1]), new HashSet<>());
+    return dfs(blockedSet, s[0], s[1], t ,new HashSet<>()) &&
+        dfs(blockedSet, t[0], t[1], s, new HashSet<>());
   }
 
-  private boolean dfs(Set<Long> blockedSet, int i, int j, long target, Set<Long> visited) {
-    if (i < 0 || i >= 1e6 || j < 0 || j >= 1e6 || blockedSet.contains(hash(i, j)) ||
-        visited.contains(hash(i, j)))
+  private boolean dfs(Set<String> blockedSet, int i, int j, int[] target, Set<String> visited) {
+     String key = i + "," + j;
+    if (i < 0 || i >= 1e6 || j < 0 || j >= 1e6 || blockedSet.contains(key) ||
+        visited.contains(key))
       return false;
 
-    visited.add(hash(i, j));
-    if (visited.size() > (1 + 199) * 199 / 2 || hash(i, j) == target)
+    visited.add(key);
+    if (visited.size() > (1 + 199) * 199 / 2 || (i == target[0] && j == target[1]))
       return true;
 
     return                                            //
