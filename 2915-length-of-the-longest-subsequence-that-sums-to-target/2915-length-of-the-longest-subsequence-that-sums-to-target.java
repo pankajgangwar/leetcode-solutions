a2460e9336b6edbdc7510d2cf6b4393c043e6997
Pankaj Kumar <pankaj.gat@gmail.com>
2024-01-21 22:26:34 +0530

    Time: 63 ms (79.55%), Space: 55.2 MB (54.31%) - LeetHub

--- 2915-length-of-the-longest-subsequence-that-sums-to-target/2915-length-of-the-longest-subsequence-that-sums-to-target.java
+++ 2915-length-of-the-longest-subsequence-that-sums-to-target/2915-length-of-the-longest-subsequence-that-sums-to-target.java
@@ -1,44 +1,23 @@
 class Solution {
-    public int lengthOfLongestSubsequence(List<Integer> list, int sum) {
-        int[] nums = list.stream().mapToInt(i-> i).toArray();
-        int n = nums.length;
-        int[][] dp = new int[n + 1][sum + 1];
-        
+    public int lengthOfLongestSubsequence(List<Integer> nums, int target) {
+        int n = nums.size();
+        int[] arr = nums.stream().mapToInt(i-> i).toArray();
+        int[][]dp = new int[n+1][target+1];
         for(int[] row : dp){
             Arrays.fill(row, -1);
         }
-        
-        for (int i = 0; i < n + 1; i++)
+        for (int i = 0; i <= n; i++) {
             dp[i][0] = 0;
-
-        for (int i = 1; i < n + 1; i++) {
-            for (int j = 1; j < sum + 1; j++) {
-                dp[i][j] = dp[i - 1][j];
-
-                if (j >= nums[i - 1] && dp[i - 1][j - nums[i - 1]] != -1)
-                    dp[i][j] = Math.max(1 + dp[i - 1][j - nums[i - 1]], dp[i][j]);
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= target ; j++) {
+                dp[i][j] = dp[i-1][j];
+                if(arr[i-1] <= j && dp[i-1][j - arr[i-1]] != -1){
+                    dp[i][j] = Math.max(dp[i][j], 1 + dp[i-1][j - arr[i-1]]);
+                }
             }
         }
-
-        return dp[n][sum];
+        return dp[n][target];
     }
     
-    public int lengthOfLongestSubsequence11(int[] nums, int sum) {
-        int n = nums.length;
-        int[][] dp = new int[n + 1][sum + 1];
-        
-        for (int i = 0; i < n + 1; i++)
-            dp[i][0] = 0;
-
-        for (int i = 1; i < n + 1; i++) {
-            for (int j = 1; j < sum + 1; j++) {
-                dp[i][j] = dp[i - 1][j];
-
-                if (j >= nums[i - 1] && dp[i - 1][j - nums[i - 1]] != -1)
-                    dp[i][j] = Math.max(1 + dp[i - 1][j - nums[i - 1]], dp[i][j]);
-            }
-        }
-
-        return dp[n][sum];
-    }
 }
