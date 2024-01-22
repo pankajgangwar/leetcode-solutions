0a78fd789a7f7fd5db596234736a19c5e970706e
Pankaj Kumar <pankaj.gat@gmail.com>
2024-01-22 19:43:09 +0530

    Time: 15 ms (86.67%), Space: 45.4 MB (46.67%) - LeetHub

--- 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.java
+++ 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.java
@@ -2,28 +2,24 @@ class Solution {
     public boolean canBeValid(String s, String l) {
         int n = s.length();
         if(n%2==1) return false;
-        int canChange = 0, open = 0, close = 0;
+        int balance = 0;
         for (int i = 0; i < n; i++) {
-            if(l.charAt(i) == '0'){
-                canChange++;
-            }else if (s.charAt(i) == '(') {
-                open++;
-            }else if(s.charAt(i) == ')'){
-                close++;
+            if(l.charAt(i) == '0' || s.charAt(i) == '('){
+                balance++;
+            }else{
+                balance--;
             }
-            if(canChange + open - close < 0) return false;
+            if(balance < 0) return false;
         }
 
-        canChange = open = close = 0;
+        balance = 0;
         for (int i = n-1; i >= 0; i--) {
-            if(l.charAt(i) == '0'){
-                canChange++;
-            }else if (s.charAt(i) == '(') {
-                open++;
-            }else if(s.charAt(i) == ')'){
-                close++;
+            if(l.charAt(i) == '0' || s.charAt(i) == ')'){
+                balance++;
+            }else{
+                balance--;
             }
-            if(canChange - open + close < 0) return false;
+            if(balance < 0) return false;
         }
         return true;
     }
