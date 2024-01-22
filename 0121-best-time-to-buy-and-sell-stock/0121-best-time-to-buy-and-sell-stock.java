8fcbb528bddd079d0dd4a2198929ff041e930579
Pankaj Kumar <pankaj.gat@gmail.com>
2024-01-22 08:39:52 +0530

    Time: 2 ms (78.38%), Space: 61.7 MB (39.86%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        return usingKadanesAlgo(prices);
+    }
+    
+    public int usingDp(int[] prices){
+        if(prices.length == 0){
+            return 0;
+        }
+        int min = prices[0];
+        int max = prices[0];
+        int n = prices.length;
+        int max_profit = 0;
+        for(int i = 1; i < n; i++){
+            
+            if(prices[i] > max){
+                max = prices[i];
+            }
+           
+            if(prices[i] < min){
+                min = prices[i];
+                max = prices[i];
+            }
+            max_profit = Math.max(max_profit, max - min);
+        }
+        return max_profit;
+    }
+    
+    
+    public int usingKadanesAlgo(int[] prices){
+        int maxCur = 0;
+        int maxSoFar = 0;
+        for(int i = 1; i < prices.length; i++){
+            maxCur = Math.max(0, maxCur += prices[i] - prices[i-1]);
+            maxSoFar = Math.max(maxCur, maxSoFar);
+        }
+        return maxSoFar;
+    }
+}
