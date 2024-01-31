1d8d7bc978de093815c8652456fe716ae19d77e5
Pankaj Kumar <pankaj.gat@gmail.com>
2024-02-01 00:00:34 +0530

    Time: 216 ms (74.34%), Space: 61 MB (5.31%) - LeetHub

--- 2183-count-array-pairs-divisible-by-k/2183-count-array-pairs-divisible-by-k.java
+++ 2183-count-array-pairs-divisible-by-k/2183-count-array-pairs-divisible-by-k.java
@@ -3,19 +3,20 @@ import java.math.BigInteger;
 class Solution {
     public long countPairs(int[] nums, int k) {
         HashMap<Long, Long> cnt = new HashMap<>();
-        long res = 0;
         for(int a : nums){
             long key = BigInteger.valueOf(a).gcd(BigInteger.valueOf(k)).longValue();
-            
-            for(long x : cnt.keySet()){
-                long count = cnt.get(x);
-                if(x * a % k == 0L ){
-                    res += count;
+            cnt.put(key, cnt.getOrDefault(key, 0l) + 1l);
+            //cnt.merge(key, 1l, Long::sum);
+        }
+        System.out.println(cnt);
+        long res = 0;
+        for(long x : cnt.keySet()){
+            for(long y : cnt.keySet()){
+                if(x <=y && x * y % k == 0L ){
+                    res += x < y ? cnt.get(x) * cnt.get(y) : cnt.get(x) * ( cnt.get(x) - 1L) / 2L;
                 }
             }
-            cnt.put(key, cnt.getOrDefault(key, 0l) + 1l);
         }
-        
         return res;
     }
 }
