26fd677a5d52f5fa1a1dcbb59cee9ebeda6cc5cc
Pankaj Kumar <pankaj.gat@gmail.com>
2024-02-04 00:00:08 +0530

    Time: 92 ms (35.29%), Space: 69.8 MB (77.11%) - LeetHub

--- 2251-number-of-flowers-in-full-bloom/2251-number-of-flowers-in-full-bloom.java
+++ 2251-number-of-flowers-in-full-bloom/2251-number-of-flowers-in-full-bloom.java
@@ -1,5 +1,29 @@
 class Solution {
     public int[] fullBloomFlowers(int[][] flowers, int[] people) {
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        int[] ans = new int[people.length];
+        map.put(0, 0);
+        for (int[] flower : flowers) {
+            map.put(flower[0], map.getOrDefault(flower[0], 0) + 1);
+            map.put(flower[1] + 1, map.getOrDefault(flower[1] + 1, 0) - 1);
+        }
+        int sum = 0;
+        List<Integer> val = new ArrayList<>();
+        for(int v : map.values()){
+            sum += v;
+            val.add(sum);
+        }
+        List<Integer> keys = new ArrayList<>(map.keySet());
+        for (int i = 0; i < people.length; i++) {
+            int p = people[i];
+            int idx = Collections.binarySearch(keys, p);
+            if(idx < 0) idx = ~idx - 1;
+            ans[i] = val.get(idx);
+        }
+        return ans;
+    }
+    
+    public int[] fullBloomFlowers1(int[][] flowers, int[] people) {
         PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
         int p = people.length;
         int[] ans = new int[p];
