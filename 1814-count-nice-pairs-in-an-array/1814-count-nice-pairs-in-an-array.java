9bf6455b393a6551851fbfa4d9e15dfe2240dbb2
Pankaj Kumar <pankaj.gat@gmail.com>
2024-02-03 11:41:25 +0530

    Time: 88 ms (11.42%), Space: 55.6 MB (78.38%) - LeetHub

--- 1814-count-nice-pairs-in-an-array/1814-count-nice-pairs-in-an-array.java
+++ 1814-count-nice-pairs-in-an-array/1814-count-nice-pairs-in-an-array.java
@@ -1,6 +1,6 @@
 class Solution {
     public int countNicePairs(int[] nums) {
-        HashMap<Long, Long> badPairsMap = new HashMap<>();
+        HashMap<Long, Long> goodPairsMap = new HashMap<>();
         
         int n = nums.length;
         long goodPairs = 0L;
@@ -9,9 +9,9 @@ class Solution {
             String rev = new StringBuilder(String.valueOf(nums[i])).reverse().toString();
             long diff = (long)nums[i] - Long.parseLong(rev);
             
-            goodPairs += badPairsMap.getOrDefault(diff, 0L);
+            goodPairs += goodPairsMap.getOrDefault(diff, 0L);
             goodPairs %= mod;
-            badPairsMap.put(diff, badPairsMap.getOrDefault(diff, 0L) + 1L);
+            goodPairsMap.put(diff, goodPairsMap.getOrDefault(diff, 0L) + 1L);
         }
         return (int)(goodPairs);
     }
