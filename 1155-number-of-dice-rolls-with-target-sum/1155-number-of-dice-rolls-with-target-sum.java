2b7fc1d9d68addf013f2956389f4471411afb77b
Pankaj Kumar <pankaj.gat@gmail.com>
2024-02-03 10:19:40 +0530

    Time: 22 ms (23.46%), Space: 41.8 MB (56.17%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
@@ -3,19 +3,17 @@ class Solution {
        return findWays(f, d, target);
     }
     
-    public static int findWays(int m, int n, int target){
-
+     public static int findWays(int faces, int n, int target){
         int[][] table = new int[n+1][target+1];
         int mod = (int)1e9+7;
-
-        for(int j = 1; j <= m && j <= target; j++)
+        for(int j = 1; j <= faces && j <= target; j++)
             table[1][j] = 1;
-
-        for(int i = 2; i <= n;i ++){
+         
+        for(int i = 2; i <= n; i++){
             for(int j = 1; j <= target; j++){
-                for(int k = 1; k < j && k <= m; k++){
+                for(int k = 1; k < j && k <= faces; k++){
                     table[i][j] += table[i-1][j-k];
-                     if(table[i][j] > mod) table[i][j] = table[i][j] % mod;
+                    table[i][j] %= mod;
                 }
             }
         }
