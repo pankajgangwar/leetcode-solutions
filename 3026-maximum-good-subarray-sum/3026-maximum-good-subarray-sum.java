8c360fffddb1bb9dfaeda66c7ca98585473d8697
Pankaj Kumar <pankaj.gat@gmail.com>
2024-02-04 09:32:24 +0530

    Time: 64 ms (100.00%), Space: 67.4 MB (50.00%) - LeetHub

--- 3026-maximum-good-subarray-sum/3026-maximum-good-subarray-sum.java
+++ 3026-maximum-good-subarray-sum/3026-maximum-good-subarray-sum.java
@@ -12,15 +12,15 @@ class Solution {
 
         long sum = 0l;
         for (int i = 0; i < n; i++) {
-            if (map.getOrDefault(nums[i], Long.MAX_VALUE) > sum) {
-                map.put(nums[i], sum);
+            if (map.getOrDefault(nums[i], Long.MAX_VALUE) > prefix[i]) {
+                map.put(nums[i], prefix[i]);
             }
             sum += nums[i];
             if (map.containsKey(nums[i] + k)) {
-                maxSum = Math.max(maxSum, sum - map.get(nums[i] + k));
+                maxSum = Math.max(maxSum, prefix[i+1] - map.get(nums[i] + k));
             }
             if (map.containsKey(nums[i] - k)) {
-                maxSum = Math.max(maxSum, sum - map.get(nums[i] - k));
+                maxSum = Math.max(maxSum, prefix[i+1] - map.get(nums[i] - k));
             }
         }
 
