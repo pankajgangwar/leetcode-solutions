001cffc664cf8c15cd5fd6eee9501cc55a70c0d4
Pankaj Kumar <pankaj.gat@gmail.com>
2024-02-04 21:15:20 +0530

    Time: 5 ms (100.00%), Space: 44.3 MB (50.00%) - LeetHub

--- 3025-find-the-number-of-ways-to-place-people-i/3025-find-the-number-of-ways-to-place-people-i.java
+++ 3025-find-the-number-of-ways-to-place-people-i/3025-find-the-number-of-ways-to-place-people-i.java
@@ -1,18 +1,16 @@
 class Solution {
-    public int numberOfPairs(int[][] points) {
-        Arrays.sort(points, (a,b) -> a[0] == b[0] ? -a[1] + b[1] : a[0] - b[0]);
-        int ans = 0;
-        for(int i = 0; i < points.length; ++i){
-            for(int j = i+1; j < points.length; ++j){
-                if(points[i][1] >= points[j][1]){
-                    boolean flag = true;
-                    for(int k = i+1; k < j; ++k){
-                        if(points[k][1] <= points[i][1] && points[k][1] >= points[j][1] ) flag = false;
-                    }
-                    if(flag) ans++;
+    public int numberOfPairs(int[][] A) {
+        int res = 0, n = A.length;
+        Arrays.sort(A, (a, b) -> a[0] == b[0] ? Integer.compare(b[1], a[1]) : Integer.compare(a[0], b[0]));
+        for (int i = 0; i < n; ++i) {
+            int y = -(int)2e9;
+            for (int j = i + 1; j < n; ++j) {
+                if (A[i][1] >= A[j][1] && A[j][1] > y) {
+                    res++;
+                    y = A[j][1];
                 }
             }
         }
-        return ans;
+        return res;
     }
 }
