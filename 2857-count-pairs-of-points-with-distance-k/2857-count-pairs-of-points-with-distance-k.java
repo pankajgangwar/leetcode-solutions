a5f12acbc004700cc6d60396b5ac263fb2adb836
Pankaj Kumar <pankaj.gat@gmail.com>
2024-02-17 08:27:39 +0530

    Time: 1245 ms (20.84%), Space: 83.9 MB (5.00%) - LeetHub

--- 2857-count-pairs-of-points-with-distance-k/2857-count-pairs-of-points-with-distance-k.java
+++ 2857-count-pairs-of-points-with-distance-k/2857-count-pairs-of-points-with-distance-k.java
@@ -3,11 +3,18 @@ class Solution {
         HashMap<String, Integer> seen = new HashMap<>();
         int total = 0;
         for(List<Integer> c1 : coordinates){
-            int x1 = c1.get(0);
-            int y1 = c1.get(1);
-            for (int xd = 0; xd <= k ; xd++) {
-                int x2 = (x1 ^ xd);
-                int y2 = (y1 ^ (k - xd));
+            int x1 = c1.get(0), y1 = c1.get(1);
+            //using a^b=c , then a^c=b
+            //c[i][0]^x+c[i][1]^y=k                   (0)
+            //Consider above is true , then
+            //if , c[i][0]^x=val                      (1)
+            //then c[i][1]^y=k-val                    (2)
+            //we have c[i][0]^val=x;
+            //we have c[i][1]^(k-val)=y;
+
+            for (int val = 0; val <= k ; val++) {
+                int x2 = (x1 ^ val);
+                int y2 = y1 ^ (k - val);
                 String key = x2 + "," + y2;
                 if(seen.containsKey(key)){
                     total += seen.getOrDefault(key, 0);
