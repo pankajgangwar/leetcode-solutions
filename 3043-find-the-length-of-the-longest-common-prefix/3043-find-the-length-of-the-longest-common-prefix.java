941925265af37fe01bc84691285c623a3b6c80b0
Pankaj Kumar <pankaj.gat@gmail.com>
2024-02-18 09:50:20 +0530

    Time: 36 ms (50.00%), Space: 55.2 MB (50.00%) - LeetHub

--- 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.java
+++ 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.java
@@ -2,23 +2,23 @@ class Solution {
     public int longestCommonPrefix(int[] arr1, int[] arr2) {
 
         TrieNode root = new TrieNode();
-        for (int i = arr2.length - 1; i >= 0 ; i--) {
-            insert(root, arr2[i]);
+        for (int i = 0; i < arr1.length ; i++) {
+            insert(root, arr1[i]);
         }
 
         int max = 0;
-        for (int i = arr1.length - 1; i >= 0 ; i--) {
-            max = Math.max(max, search(root, arr1[i]));
+        for (int i = 0; i < arr2.length ; i++) {
+            max = Math.max(max, search(root, arr2[i]));
         }
         
-        root = new TrieNode();
+        /*root = new TrieNode();
         for (int i = arr1.length - 1; i >= 0 ; i--) {
             insert(root, arr1[i]);
         }
 
         for (int i = arr2.length - 1; i >= 0 ; i--) {
             max = Math.max(max, search(root, arr2[i]));
-        }
+        }*/
         return max;
     }
     
@@ -33,15 +33,17 @@ class Solution {
     public int search(TrieNode root, int n){
         String s = String.valueOf(n);
         TrieNode r = root;
+        int len = 0;
         for (int i = 0; i < s.length(); i++) {
             int d = (int)s.charAt(i) - '0';
             if(r.children[d] != null){
                 r = r.children[d];
+                len++;
             }else{
-                return i;
+                break;
             }
         }
-        return r.isEndOfWord ? s.length() : 0;
+        return len;
     }
 
     public void insert(TrieNode root, int n){
