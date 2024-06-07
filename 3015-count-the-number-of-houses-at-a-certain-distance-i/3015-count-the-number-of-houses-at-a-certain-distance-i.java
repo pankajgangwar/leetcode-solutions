98be1b81a430e123463595e8717a74e13aa830d5
Pankaj Kumar <pankaj.gat@gmail.com>
2024-06-07 17:14:06 +0530

    Time: 171 ms (11.70%), Space: 44.8 MB (65.96%) - LeetHub

--- 3015-count-the-number-of-houses-at-a-certain-distance-i/3015-count-the-number-of-houses-at-a-certain-distance-i.java
+++ 3015-count-the-number-of-houses-at-a-certain-distance-i/3015-count-the-number-of-houses-at-a-certain-distance-i.java
@@ -1,5 +1,9 @@
 class Solution {
+    
     public int[] countOfPairs(int n, int x, int y) {
+        return floydWarshall(n, x, y);
+    }
+    public int[] bfsSol(int n, int x, int y) {
         int[] res = new int[n];
         List<Integer>[] adj = new ArrayList[n + 1];
         for(int i = 1; i <= n; i++) {
@@ -14,6 +18,46 @@ class Solution {
 
         for (int i = 1; i <= n; i++) {
             runBfs(i, res, adj );
+        }
+        return res;
+    }
+    
+     public int[] floydWarshall(int n, int x, int y) {
+        int[][] dist = new int[n + 1][n + 1];
+        for(int i = 1; i <= n; i++){
+            Arrays.fill(dist[i], Integer.MAX_VALUE);
+            dist[i][i] = 0;
+            if(i < n){
+                dist[i][i+1] = 1;
+            }
+            if(i > 0){
+                dist[i][i-1] = 1;
+            }
+        }
+        dist[x][y] = dist[y][x] = 1;
+
+        for(int k = 1; k <= n; k++){
+            for (int i = 1; i <= n ; i++) {
+                for (int j = 1; j <= n ; j++) {
+                    if(dist[i][k] != Integer.MAX_VALUE && 
+                         dist[k][j] != Integer.MAX_VALUE){
+                        dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+                    }
+                }
+            }
+        }
+        Map<Integer, Integer> countMap = new HashMap<>();
+        for(int i = 1; i <= n; i++){
+            for (int j = 1; j <= n ; j++) {
+                if(dist[i][j] != Integer.MAX_VALUE && (i != j)){
+                    countMap.put(dist[i][j], countMap.getOrDefault(dist[i][j], 0) + 1);
+                }
+            }
+        }
+
+        int[] res = new int[n];
+        for (int k = 1; k <= n ; k++) {
+            res[k-1] = countMap.getOrDefault(k, 0);
         }
         return res;
     }
